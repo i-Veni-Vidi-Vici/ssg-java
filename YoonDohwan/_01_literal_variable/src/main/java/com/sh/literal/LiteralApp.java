fdc7341fb3ce914541fc4e966373f6b8f970a163
dohwani <stc1015@naver.com>
2024-03-07 17:11:33 +0900

    literl+variable

---
+++ YoonDohwan/_01_literal_variable/src/main/java/com/sh/literal/LiteralApp.java
@@ -0,0 +1,32 @@
+package com.sh.literal;
+
+public class LiteralApp {
+
+    /**
+     * (asterisk)
+     *
+     * Literal(값)
+     * @param args
+     */
+    public static void main(String[]args){
+       //1.정수
+        int a=1; int b=1;
+       System.out.println(123);
+       System.out.println(-123);
+       //실수
+       System.out.println(12.345);
+       //논리값 (참거짓)
+       System.out.println(true);
+       System.out.println(false);
+       //문자(1개) -작은따움표로 감싸서 표현 ""
+        System.out.println('a'); //sout 단축키
+        System.out.println("a");
+        System.out.println(a+b);
+        // 문자열 0~n개
+        System.out.println("안녕");
+        System.out.println(""); //빈문자연 표현가능
+
+
+
+    }
+}
