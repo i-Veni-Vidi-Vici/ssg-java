988abb540c885e716d3fbf3cc3597348d00762de
CstoneKu <mskoo0725@gmail.com>
2024-03-08 17:17:54 +0900

    Feat: 상수, 변수, 연산자 추가

---
+++ KuMinsang/_02_operator/src/main/java/com/sh/operator/logical/LogicalOperatorApp2.java
@@ -0,0 +1,20 @@
+package com.sh.operator.logical;
+
+/**
+ * 논리 연산자 진리표
+ *
+ */
+
+public class LogicalOperatorApp2 {
+    public static void main(String[] args) {
+        int num = 10;
+        //System.out.println(true && num++ > 0); //true , a = 11 출력
+        System.out.println(false && num++ > 0); //우항을 실행하지 않음 a = 10 출력
+        System.out.println(num); //11 또는 10
+
+        int k = 20;
+        //System.out.println(true|| k++ > 20); //우항 검사를 하지 않음 true a = 20
+        System.out.println(false|| k++ > 20);   //우항을 실행함 false 21
+        System.out.println(k);
+    }
+}
