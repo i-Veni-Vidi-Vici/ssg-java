15cb399aaa04e70db9ecd857e26113fcad017d4f
dhgudehd98 <ohd7150@nate.com>
2024-03-08 23:56:56 +0900

    feat : 연산자 실습문제2 추가

--- OhHyungDong/_02_operator/src/main/java/com/sh/operator/operator/App1.java
+++ OhHyungDong/_02_operator/src/main/java/com/sh/operator/operator/App1.java
@@ -12,7 +12,7 @@ public class App1 {
         int name1Total = name1Korean + name1English + name1Math;
         double name1Avg = name1Total / 3;
 
-        // 세 과목의 점수가 각각 40점 이상이면서 , 평균이 60점이상이면 True 반환, 둘 중 하나라도 만족하지 못하면 False 반환
+        // 세 과목의 점수가 각각 40점 이상이면서 , 평균이 60점이상이면 True 반환, 둘 중 하나라도 만족하지 못하면 False 반환.
         boolean name1IsSuccessed = (name1Korean >= 40 && name1English >=40 && name1Math >= 40) && (name1Avg >= 60);
         System.out.print("이름 : " + name1 + " 총점 : " + name1Total + " 평균 : " + name1Avg );
         System.out.println(name1IsSuccessed ? " [합격]" : " [불합격]");
@@ -25,6 +25,7 @@ public class App1 {
         int name2Total = name2Korean + name2English + name2Math;
         double name2Avg = name2Total / 3;
 
+        // 세 과목의 점수가 각각 40점 이상이면서 , 평균이 60점이상이면 True 반환, 둘 중 하나라도 만족하지 못하면 False 반환.
         boolean name2IsSuccessed = (name2Korean >= 40 && name2English >=40 && name2Math >= 40) && (name2Avg >= 60);
         System.out.print("이름 : " + name2 + " 총점 : " + name2Total + " 평균 : " + name2Avg );
         System.out.println(name2IsSuccessed ? " [합격]" : " [불합격]");
