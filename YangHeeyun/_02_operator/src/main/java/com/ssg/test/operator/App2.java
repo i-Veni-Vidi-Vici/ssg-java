25a50887c066c1d8d57c39ccab4144ca781152a0
vlshzl35 <>
2024-03-11 20:46:40 +0900

    Feat: logicalApp 추가

---
+++ YangHeeyun/_02_operator/src/main/java/com/ssg/test/operator/App2.java
@@ -0,0 +1,21 @@
+package com.ssg.test.operator.logical;
+
+public class App2 {
+    public static void main(String[] args){
+        String name = "홍길동";
+        String phoneNum = "01012341234";
+        char sex = 'M';
+        String address = "서울시 서초구 방배동";
+
+        System.out.println("이 름  전화번호   성별  주소");
+        System.out.println("----------------------------------------");
+        System.out.println(name + " " + phoneNum + " " + (sex=='M'?"남":"여") + " " + address);
+
+        name = "신사임당";
+        phoneNum = "01044443333";
+        sex = 'F';
+        address = "경기도 남양주시 별내면";
+
+        System.out.println(name + " " + phoneNum + " " + (sex=='M'?"남":"여") + " " + address);
+    }
+}
