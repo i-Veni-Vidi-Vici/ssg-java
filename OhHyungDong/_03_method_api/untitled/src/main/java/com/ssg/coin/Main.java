f18bb9fa78c3888d00e6703ddd93d1512d3b07f4
dhgudehd98 <ohd7150@nate.com>
2024-03-12 21:36:06 +0900

    Feat : 3월 12일 과제 실습문제 1번,2번, 동전문제 추가

---
+++ OhHyungDong/_03_method_api/untitled/src/main/java/com/ssg/coin/Main.java
@@ -0,0 +1,11 @@
+package com.ssg.coin;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.flipCoin();
+    }
+}
