f4e6b3a456ac065379efd23edaf20ead5c7ab617
CstoneKu <mskoo0725@gmail.com>
2024-03-12 17:19:57 +0900

    feature: method 패키지 추가

---
+++ KuMinsang/_03_method_api/src/main/java/com/sh/method/App2.java
@@ -0,0 +1,25 @@
+package com.sh.method;
+
+/**
+ * ssg java 출력
+ */
+
+public class App2 {
+    public static void main(String[] args) {
+        App2 app2 = new App2();
+        app2.s();
+        app2.s();
+        app2.g();
+        app2.j();
+        app2.a();
+        app2.v();
+        app2.a();
+    }
+
+    public void a() {System.out.println("a");}
+    public void g() {System.out.println("g");}
+    public void j() {System.out.println("j");}
+    public void s() {System.out.println("s");}
+    public void v() {System.out.println("v");}
+
+}
