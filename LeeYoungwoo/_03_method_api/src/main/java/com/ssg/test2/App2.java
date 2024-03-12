69f7d7496dc56f9d3f2b9b5aec80792a8f8785bd
BOOK-QFJTSFASTP\leedu <leeduddn98@gmail.com>
2024-03-12 22:17:03 +0900

    Feat: 메소드/난수 실습문제 제출

--- LeeYoungwoo/_03_method_api/src/main/java/com/ssg/test2/App2.java
+++ LeeYoungwoo/_03_method_api/src/main/java/com/ssg/test2/App2.java
@@ -2,6 +2,6 @@ package com.ssg.test2;
 
 public class App2 {
     public static void main(String[] args) {
-        
+        RandomUtils.generate();
     }
 }
--- LeeYoungwoo/_03_method_api/src/main/java/com/ssg/test2/RandomUtils.java
+++ LeeYoungwoo/_03_method_api/src/main/java/com/ssg/test2/RandomUtils.java
@@ -3,10 +3,14 @@ package com.ssg.test2;
 import java.util.Random;
 
 public class RandomUtils {
-    public void generate() {
+    public static void generate() {
         Random rnd = new Random();
+        // 정수 범위의 난수를 생성
         int answer = rnd.nextInt();
+        // 난수 출력
         System.out.println(answer);
+        // 홀수인지 짝수인지 출력
+        System.out.println(answer % 2 == 0 ? "짝수입니다" : "홀수입니다" );
 
     }
 }
