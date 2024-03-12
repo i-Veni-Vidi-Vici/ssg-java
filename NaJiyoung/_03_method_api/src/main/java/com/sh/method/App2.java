8dfe922db51727dd8271cb0cbe15565ac5e67f44
jyna120 <jyna120@naver.com>
2024-03-12 17:22:54 +0900

    Feat: 메소드, package/import, Math API, Random API, Scanner API

---
+++ NaJiyoung/_03_method_api/src/main/java/com/sh/method/App2.java
@@ -0,0 +1,26 @@
+package com.sh.method;
+
+/**
+ * ssg java 출력
+ */
+public class App2 {
+    public static void main(String[] args) {
+        // 1. 객체생성
+        App2 app2 = new App2();
+        // 2. 객체를 통해 메소드 호출
+        app2.s();
+        app2.s();
+        app2.g();
+        System.out.print(" ");
+        app2.j();
+        app2.a();
+        app2.v();
+        app2.a();
+    }
+
+    public void a() {System.out.print("a");}
+    public void g() {System.out.print("g");}
+    public void j() {System.out.print("j");}
+    public void s() {System.out.print("s");}
+    public void v() {System.out.print("v");}
+}
