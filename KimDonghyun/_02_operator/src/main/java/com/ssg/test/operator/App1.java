e2ae20680dc210795cf5abce4da429d602ca2f2e
shqkel <shqkel1863@gmail.com>
2024-03-12 17:20:43 +0900

    Feat: 메소드, package/import, Math API, Random API, Scanner API

--- KimDonghyun/_02_operator/src/main/java/com/ssg/test/operator/App1.java
+++ KimDonghyun/_02_operator/src/main/java/com/ssg/test/operator/App1.java
@@ -3,7 +3,7 @@ package com.ssg.test.operator;
 public class App1 {
     public static void main(String[] args) {
         String name;
-        int kor, eng, math, total;
+        int kor, eng, math, total; // 같은 타입 n개 변수를 선언할 때
         double avg;
 
         // data1
@@ -15,7 +15,7 @@ public class App1 {
         avg = total / 3.0; // 3으로 나누면 결과값이 int라서 소수점이하를 구할수 없다.
 
         boolean eachSubjectPassed = kor >= 40 && eng >= 40 && math >= 40;
-        boolean avgPassed = avg > 60;
+        boolean avgPassed = avg >= 60;
         System.out.println(name + " : 총점 = " + total + ", 평균 = " + avg + ", 합격여부 = " + (eachSubjectPassed && avgPassed ? "합격" : "불합격"));
 
         // data2
