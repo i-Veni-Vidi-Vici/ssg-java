e38ca5ec5a0bb5a2b0bd70604ef1f302ac48f8c4
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-13 10:57:27 +0900

    feat(_04_control_flow): create package

---
+++ ByeonSeongil/_04_control_flow/src/main/java/com/sh/condition/_if/App.java
@@ -0,0 +1,13 @@
+package com.sh.condition._if;
+
+/**
+ * 코드의 성격별로 클래스/메서드등으로 나누어 작성해야 유지보수가 수월하다.
+ * 실행 클래스 - main 메소드를 가진 클래스
+ * 기능 제공용 클래스
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
