453ffac01c4c404af31fb3716918ca3c068ac720
cstangga <cstangga@naver.com>
2024-03-13 17:44:18 +0900

    실습문제 1,2,3 + 가위바위보

--- ChoiChanguk/_04_control_flow/src/main/java/com/sh/condition/_if/If.java
+++ ChoiChanguk/_04_control_flow/src/main/java/com/sh/condition/_if/If.java
@@ -18,14 +18,17 @@ public class If {
         if (num%2==0) {
             System.out.println("짝수 입니다");
         }
-        else {
+
+        if(num%2!=0) {
             System.out.println(" 홀수 입니다 ");
         }
 
         if(num<0) {
             System.out.println("음수 입니다");
         }
-        else System.out.println("양수 입니다");
+        if(num>0) {
+            System.out.println("양수 입니다");
+        }
 
     }
 
---
+++ ChoiChanguk/_04_control_flow/src/main/java/com/ssg/test/condition/_if/Test3.java
@@ -0,0 +1,44 @@
+package com.ssg.test.condition._if;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        double height;
+        double weight;
+        double BMI;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("체중입력(kg) : ");
+        weight=sc.nextInt();
+
+        System.out.print("신장입력(m) : ");
+        height=sc.nextInt();
+        height=height*0.01;
+
+        BMI=(double) weight/(height*height);
+
+        System.out.println("--------------------------");
+        System.out.println("BMI 지수 : "+BMI);
+
+        if(BMI<18.5)
+        {
+            System.out.println("저체중입니다");
+        }
+        else if(BMI>=18.5&&BMI<23)
+        {
+            System.out.println("정상체중입니다");
+        }
+        else if(BMI>=23&&BMI<25)
+        {
+            System.out.println("과제충입니다");
+        }
+        else if(BMI>=25&&BMI<30)
+        {
+            System.out.println("비만입니다");
+        }
+        else {
+            System.out.println("고도비만입니다");
+        }
+    }
+}
