2405cef7510f8b95aba7ede2410a8b102bc97b1c
hurryduck <tigeryck0812@gmail.com>
2024-03-13 17:39:23 +0900

    Feat: 조건문 if 실습문제

---
+++ KimYeonChan/_04_control_flow/src/main/java/com/ssg/test/condition/_if/Test3.java
@@ -0,0 +1,33 @@
+package com.ssg.test.condition._if;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public void test() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("체중입력(kg): ");
+        int weight = sc.nextInt();
+        System.out.print("키입력(cm): ");
+        double height = sc.nextDouble();
+
+        System.out.println("--------------------------");
+        double resultBMI = weight / ((height * 0.01) * (height * 0.01));
+        System.out.println("BMI 지수: " + resultBMI);
+
+        if (resultBMI >= 30) {
+            System.out.println("고도비만입니다.");
+        }
+        else if (resultBMI >= 25) {
+            System.out.println("비만입니다.");
+        }
+        else if (resultBMI >= 23) {
+            System.out.println("과체중입니다.");
+        }
+        else if (resultBMI >= 18.5) {
+            System.out.println("정상체중입니다.");
+        }
+        else {
+            System.out.println("저체중입니다.");
+        }
+    }
+}
