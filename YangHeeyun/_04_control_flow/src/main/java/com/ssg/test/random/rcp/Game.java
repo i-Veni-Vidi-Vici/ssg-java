779778722e13641bfaa334176a4f20b8c22b3d2d
vlshzl35 <vlshzl35@naver.com>
2024-03-13 17:55:26 +0900

    7일차 과제 풀다만거

---
+++ YangHeeyun/_04_control_flow/src/main/java/com/ssg/test/random/rcp/Game.java
@@ -0,0 +1,49 @@
+/*
+package com.ssg.test.random.rcp;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public void rockPaperScissors(){
+        Scanner sc = new Scanner();
+        Random rnd = new Random();
+
+        System.out.println("===가위 바위 보 게임===");
+        System.out.print("숫자를 선택하세요(1.가위/2.바위/3.보) : ");
+        int num = sc.nextInt();
+
+        if(randomNum == 1){
+            com = "가위";
+        }else if(randomNum == 2){
+            com = "바위";
+        }else if(randomNum == 3){
+            com = "보";
+        }
+
+        // 컴퓨터도 가위,바위,보 해야되니까 random으로 1~3까지 받고 출력문으로 가위바위보 구분
+        int randomNum = rnd.nextInt(3) + 1;
+        String com = "";
+
+        if(randomNum == 1){
+            com = "가위";
+        }else if(randomNum == 2){
+            com = "바위";
+        }else if(randomNum == 3){
+            com = "보";
+        }
+
+        System.out.println("=======결과=======");
+        System.out.println("당신은 " + num + "를 냈습니다.");
+        System.out.println("컴퓨터는 " + com + "를 냈습니다.");
+
+        if ((num == "가)
+        }
+        System.out.println("=================");
+        System.out.println("당신이 이겼습니다.ㅠㅠ");
+        System.out.println("당신이 졌습니다!");
+
+    }
+}
+*/
