cf49c969469df3a19b902f6ea6a6164f74ca43f6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-13 09:15:42 +0900

    feat(ByeonSeongil): create 실습문제1-1

---
+++ ByeonSeongil/_03_method_api/src/main/java/com/ssg/test1/App1.java
@@ -0,0 +1,8 @@
+package com.ssg.test1;
+
+public class App1 {
+    public static void main(String[] args) {
+        RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
+        randomNumGenerator.generate();
+    }
+}
