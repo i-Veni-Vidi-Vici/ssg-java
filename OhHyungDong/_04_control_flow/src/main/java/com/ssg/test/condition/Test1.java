ddb8780f2fa92483c18701a12607eda098cd59ab
dhgudehd98 <ohd7150@nate.com>
2024-03-13 17:43:43 +0900

    feat : 조건문 실습문제 1, 2, 3 + 가위바위보 문제 추가

---
+++ OhHyungDong/_04_control_flow/src/main/java/com/ssg/test/condition/Test1.java
@@ -0,0 +1,46 @@
+package com.ssg.test.condition;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+    public void test()
+    {
+        Scanner scan = new Scanner(System.in);
+        int result = 0;
+
+        System.out.print("정수1 입력 : ");
+        int firstNumber = scan.nextInt();
+        System.out.print("정수2 입력 : ");
+        int secondNumber = scan.nextInt();
+        System.out.print("연산자 입력(+ - * / : ");
+        String operator = scan.next();
+        System.out.println("-------------------");
+
+        switch (operator)
+        {
+            case"+":
+                result = firstNumber + secondNumber;
+                break;
+             case"-":
+                result = firstNumber - secondNumber;
+                break;
+             case"*":
+                result = firstNumber * secondNumber;
+                break;
+             case"/":
+                result = firstNumber / secondNumber;
+                break;
+             case"%":
+                result = firstNumber % secondNumber;
+                break;
+            default :
+                System.out.println("잘못 입력하셨습니다. 프로그램을 종료합니다.");
+                return;
+
+
+        }
+        System.out.println(firstNumber + operator + secondNumber + "=" + result);
+    }
+
+}
