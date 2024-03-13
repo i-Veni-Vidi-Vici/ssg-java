841ce03a11809717fb0c91497bb5caee7b2dac7f
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-13 17:29:11 +0900

    feat(Test2): create 조건문 실습 문제2

--- ByeonSeongil/_04_control_flow/src/main/java/com/ssg/test/condition/_if/App.java
+++ ByeonSeongil/_04_control_flow/src/main/java/com/ssg/test/condition/_if/App.java
@@ -3,6 +3,8 @@ package com.ssg.test.condition._if;
 public class App {
     public static void main(String[] args) {
         Test1 test1 = new Test1();
-        test1.test();
+//        test1.test();
+        Test2 test2 = new Test2();
+        test2.test();
     }
 }
