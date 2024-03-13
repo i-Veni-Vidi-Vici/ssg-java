9c42c0bb06d2ee734945ca68bc43b64d56033dd9
jeongeun1 <wjddms6188@gmail.com>
2024-03-13 10:02:39 +0900

    Feat: 과제, scanner

---
+++ ParkJeongeun/_03_method_api/src/main/java/com/ssg/test1/App1.java
@@ -0,0 +1,11 @@
+package com.ssg.test1;
+
+public class App1 {
+    // 실행클래스 : main 메소드가 있는 클래스
+    // 기능제공 클래스에서 코드 작성
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ ParkJeongeun/_03_method_api/src/main/java/com/ssg/test1/RandomNumGenerator.java
@@ -0,0 +1,13 @@
+package com.ssg.test1;
+
+public class RandomNumGenerator {
+
+//    구현 내용 :
+//    `generate():void`
+//      1. 1에서 1000사이의 정수범위의 난수를 생성
+//      2. 난수출력, 생성된 난수가 300에서 600사이에 포함되는지 여부 출력
+
+
+
+
+}
