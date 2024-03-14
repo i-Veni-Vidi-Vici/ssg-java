58da97b9c0e96936edc54c5ec8e6c000060739b1
jyna120 <jyna120@naver.com>
2024-03-14 23:28:11 +0900

    Feat: 반복문 실습문제3,4 추가

--- NaJiyoung/_04_control_flow/src/main/java/com/ssg/test/loop/Test3.java
+++ NaJiyoung/_04_control_flow/src/main/java/com/ssg/test/loop/Test3.java
@@ -13,10 +13,11 @@ public class Test3 {
         System.out.print("단수 입력 : ");
         int num = sc.nextInt(); // 입력받은 정수
 
-        if(num>=1 && num <10) { // 1 ~ 9까지 가능
-            for(int i=0; i<9; i++) {
-                
+        if(num>=1 && num <=9) { // 1 ~ 9까지 가능
+            for(int i=1; i<=9; i++) {
+                System.out.printf("%d * %d = %d\n", num, i, num*i);
             }
-        }
+        } else
+            System.out.println("잘못 입력하셨습니다. 프로그램을 종료합니다.");
     }
 }
