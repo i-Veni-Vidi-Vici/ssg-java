76301f20fdfdc81d81a4999ad499db54a59ab061
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-14 16:23:03 +0900

    feat(branching): create how to use break - Break

---
+++ ByeonSeongil/_04_control_flow/src/main/java/com/sh/branching/App.java
@@ -0,0 +1,12 @@
+package com.sh.branching;
+
+
+public class App {
+    public static void main(String[] args) {
+        Break _break = new Break();
+//        _break.test1();
+        _break.test2();
+    }
+
+
+}
