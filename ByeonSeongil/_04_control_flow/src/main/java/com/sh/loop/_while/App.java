3323552d3456996545748edeb69fa44c2eb202e2
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-14 15:40:07 +0900

    feat(_while): create how to use while - While

---
+++ ByeonSeongil/_04_control_flow/src/main/java/com/sh/loop/_while/App.java
@@ -0,0 +1,12 @@
+package com.sh.loop._while;
+
+public class App {
+    public static void main(String[] args) {
+        While _while = new While();
+//        _while.test1();
+//        _while.test2();
+            _while.test4();
+        StringBuilder sb = new StringBuilder();
+        sb.toString();
+    }
+}
