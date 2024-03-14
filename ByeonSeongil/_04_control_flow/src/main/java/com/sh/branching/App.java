7b2d07a43502bc7a5c8e28109fe5f024b09dec4e
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-14 16:47:42 +0900

    feat(branching): create how to continue - Continue

--- ByeonSeongil/_04_control_flow/src/main/java/com/sh/branching/App.java
+++ ByeonSeongil/_04_control_flow/src/main/java/com/sh/branching/App.java
@@ -5,7 +5,9 @@ public class App {
     public static void main(String[] args) {
         Break _break = new Break();
 //        _break.test1();
-        _break.test2();
+//        _break.test2();
+        Continue _continue =new Continue();
+        _continue.test2();
     }
 
 
