44d4a56acfd5f266b4d882188f165b2c093b4286
dhgudehd98 <ohd7150@nate.com>
2024-03-15 18:32:52 +0900

    feat : 다차원 배열 실습문제 추가

--- OhHyungDong/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/Sample.java
+++ OhHyungDong/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/Sample.java
@@ -2,11 +2,10 @@ package com.ssg.test.multi.dimentional.array.test;
 
 public class Sample {
     public static void main(String[] args) {
-        S
-    }
+            Project1 project1 = new Project1();
+            //project1.exercise1();
 
-    public void exercise1()
-    {
-
+            Project2 project2 = new Project2();
+            project2.exercise2();
     }
 }
