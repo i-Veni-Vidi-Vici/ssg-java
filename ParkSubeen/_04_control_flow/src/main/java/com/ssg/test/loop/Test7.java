a7e88d86fb2ecb5169657b3f6dc36e38156a8616
subkka <beeniaa@naver.com>
2024-03-15 16:39:42 +0900

    Feat: 과제 1-6(미완), 1-7(수정필요)

---
+++ ParkSubeen/_04_control_flow/src/main/java/com/ssg/test/loop/Test7.java
@@ -0,0 +1,23 @@
+package com.ssg.test.loop;
+
+public class Test7 {
+    public static void main(String[] args) {
+        // 임의 변수 선언해서 그 변수로 값 조절해야함
+        for(int i =1; i < 10; i++) {
+            for(int a = 9; a > i; a--) {
+                System.out.print(" ");
+            }
+            for(int b = 0; b < i; b++) {
+                System.out.print(b+1);
+            }
+            System.out.print(" X 8 + " + i + " = ");
+            for(int a = 9; a > 0; a--) {
+                System.out.print(a);
+                if(a == 10 - i) {
+                    break;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
