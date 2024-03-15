6bbec51521b71fa6644c084ae80396b367d436ad
ooANAoo <rkawkaos77@naver.com>
2024-03-15 17:26:51 +0900

    feat 다중 배열 , 배열

--- KimDohyun/homework/src/main/java/com/ssg/App2.java
+++ KimDohyun/homework/src/main/java/com/ssg/App2.java
@@ -7,11 +7,16 @@ public class App2 {
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
+        if(n % 2 == 0) {
+            System.out.println("잘못된 숫자입니다");
+            return;
+        }
         int cnt = 0;
+
         for(int i = 0; i < n; ++i){
             if(i <= n/2) System.out.print(++cnt);
-            else System.out.println(--cnt);
-            System.out.println();
+            else System.out.print(--cnt);
+            System.out.print(" ");
         }
     }
 }
