a42513c61d99e595b4c73d1f7797ab1d89c103f2
kimmame <kimmame@naver.com>
2024-03-15 16:36:39 +0900

    Feat: 배열 추가

---
+++ KimHonghyeon/_05_array/src/main/java/com/sh/multi/Dimension/App.java
@@ -0,0 +1,18 @@
+package com.sh.multi.Dimension;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        MultiDimentionalArray mda = new MultiDimentionalArray();
+        //mda.test2();
+        //mda.test3();
+        //mda.test5();
+        JaggedArray ja = new JaggedArray();
+        //ja.test1();
+        ja.test2();
+    }
+
+
+
+}
