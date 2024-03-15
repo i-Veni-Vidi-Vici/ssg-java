17d58144cff2c9864f522e9bff56f2b218cbbce0
jectgenius <jkl9087@naver.com>
2024-03-15 18:01:49 +0900

    Update: 배열 실습문제4 수정

---
+++ SimJaeram/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/App.java
@@ -0,0 +1,8 @@
+package com.ssg.test.multi.dimentional.array.test;
+
+public class App {
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+        sample.exercise1();
+    }
+}
--- SimJaeram/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/Sample.java
+++ SimJaeram/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/Sample.java
@@ -1,5 +1,8 @@
 package com.ssg.test.multi.dimentional.array.test;
 
 public class Sample {
+    public void exercise1() {
+        int[][] arr = new int[3][5];
 
+    }
 }
