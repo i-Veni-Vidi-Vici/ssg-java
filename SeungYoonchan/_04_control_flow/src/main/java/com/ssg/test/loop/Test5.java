3e2fa37cb33ec9107cb0151f55186ab586b48ae3
Keryzy <ycseung00@gmail.com>
2024-03-15 17:51:12 +0900

    Feat: 반복문, 분기문 실습문제

--- SeungYoonchan/_04_control_flow/src/main/java/com/ssg/test/loop/Test5.java
+++ SeungYoonchan/_04_control_flow/src/main/java/com/ssg/test/loop/Test5.java
@@ -3,66 +3,73 @@ package com.ssg.test.loop;
 import java.util.Scanner;
 
 public class Test5 {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Test5 app = new Test5();
+        app.test();
+    }
+    private void test() {
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+        int age = 0;
+        String addr = "";
+        int height = 0;
+        int weight = 0;
+        String phone = "";
 
-        // 회원 정보 입력 받기
-        Member member1 = inputInfo(scanner, 1);
-        Member member2 = inputInfo(scanner, 2);
-        Member member3 = inputInfo(scanner, 3);
+        int memberCnt = 3;
 
-        // 평균 연령, 평균 키, 평균 몸무게 계산
-        int avgAge = (int)((member1.age + member2.age + member3.age) / 3.0);
-        int avgHeight = (int)((member1.height + member2.height + member3.height) / 3.0);
-        int avgWeight = (int)((member1.weight + member2.weight + member3.weight) / 3.0);
+        String result = "=================== 저장회원 ==================\n";
 
-        // 회원 정보 출력
-        System.out.println("=================== 회원목록 ================");
-        printInfo(member1);
-        printInfo(member2);
-        printInfo(member3);
-        System.out.println("===========================================");
-        // 평균값 출력
-        System.out.println("\n평균 연령: " + avgAge + "평균 키: " + avgHeight + "평균 몸무게: " + avgWeight);
+        // 기능추가1 : 총합/평균구하기
+        int sumAge = 0;
+        int sumHeight = 0;
+        int sumWeight = 0;
+
+        double avgAge = 0;
+        double avgHeight = 0;
+        double avgWeight = 0;
+
+        for (int i = 0; i < memberCnt; i++) {
+            System.out.println("-------------- " + (i + 1) + " -----------------");
+
+            System.out.print("이름을 입력하세요 : ");
+            name = sc.next();
+
+            System.out.print("나이를 입력하세요 : ");
+            age = sc.nextInt();
+
+            sc.nextLine();
+            System.out.print("주소를 입력하세요 : ");
+            addr = sc.nextLine();
+
+            System.out.print("키를 입력하세요 : ");
+            height = sc.nextInt();
+
+            System.out.print("몸무게를 입력하세요 : ");
+            weight = sc.nextInt();
+
+            System.out.print("연락처를 입력하세요 : ");
+            phone = sc.next();
+
+            result += (i + 1) + "\t" + name + "\t" + age + "\t" + addr + "\t" + height + "cm\t" + weight + "kg\t"
+                    + phone + "\n";
+
+            // 기능추가1 : 총합/평균구하기
+            sumAge += age;
+            sumHeight += height;
+            sumWeight += weight;
+        }
+
+        avgAge = (double) sumAge / memberCnt;
+        avgHeight = (double) sumHeight / memberCnt;
+        avgWeight = (double) sumWeight / memberCnt;
+
+        result += "===========================================\n";
+
+        System.out.println(result);
+        System.out.printf("평균나이 : %.1f세 | 평균신장 : %.1fcm | 평균몸무게 : %.1fkg", avgAge, avgHeight, avgWeight);
+
     }
 
-    public static Member inputInfo(Scanner scanner, int num) {
-        System.out.println("\n" + num + "번째 회원 정보를 입력하세요:");
-        System.out.print("이름: ");
-        String name = scanner.nextLine();
-        System.out.print("나이: ");
-        int age = Integer.parseInt(scanner.nextLine());
-        System.out.print("주소: ");
-        String address = scanner.nextLine();
-        System.out.print("키(cm): ");
-        double height = Double.parseDouble(scanner.nextLine());
-        System.out.print("몸무게(kg): ");
-        double weight = Double.parseDouble(scanner.nextLine());
-        System.out.print("연락처: ");
-        String contact = scanner.nextLine();
-
-        return new Member(name, age, address, height, weight, contact);
-    }
-
-    public static void printInfo(Member member) {
-        System.out.println(member.name + member.age + member.address + member.height + member.weight + member.phoneNumber);
-    }
-}
-
-class Member {
-    String name;
-    int age;
-    String address;
-    double height;
-    double weight;
-    String phoneNumber;
-
-    public Member(String name, int age, String address, double height, double weight, String contact) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-        this.height = height;
-        this.weight = weight;
-        this.phoneNumber = contact;
-    }
 }
--- SeungYoonchan/_04_control_flow/src/main/java/com/ssg/test/loop/Test6.java
+++ SeungYoonchan/_04_control_flow/src/main/java/com/ssg/test/loop/Test6.java
@@ -1,54 +1,113 @@
 package com.ssg.test.loop;
 
 import java.util.Scanner;
-
 public class Test6 {
-    public static void main(String[] args){
-        showMenu();
-        selectMenu();
-        addOrder();
+
+    public static void main(String[] args) {
+        Test6 app = new Test6();
+        app.test();
     }
 
-    private static void addOrder() {
+    private void test() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("추가 주문하시겠습니까? (y/n) : ");
-        char add = sc.nextLine().charAt(0);
-        if(add=='y'){
-            selectMenu();
-        }
-        else{
-            finishMenu();
-        }
+        String menu = "김밥류 ===================\n"
+                + "1.원조김밥===========1500원\n"
+                + "2.치즈김밥===========2000원\n"
+                + "3.참치김밥===========2300원\n"
+                + "라면류 ===================\n"
+                + "4.그냥라면===========3000원\n"
+                + "5.치즈라면===========3500원\n"
+                + "6.짬뽕라면===========4000원\n"
+                + "분식류 ===================\n"
+                + "7.떡볶이=============2500원\n"
+                + "8.순대==============2500원\n"
+                + "9.오뎅==============1000원\n"
+                + "기타 ====================\n"
+                + "10.음료수===========1000원\n";
+        char moreOrder = ' ';
+        String order = "";
+        int sum = 0;
+
+        do {
+            System.out.print(menu);
+            System.out.print("메뉴선택 => ");
+            String menuNum = sc.next();
+
+            System.out.print("수량선택 => ");
+            int count = sc.nextInt();
+
+            int price = 0;
+
+            switch (menuNum) {
+                case "1":
+                    price = 1500 * count;
+                    sum += price;
+                    order += "원조김밥: " + count + "개 - " + price + "원";
+                    break;
+                case "2":
+                    price = 2000 * count;
+                    sum += price;
+                    order += "치즈김밥: " + count + "개 - " + price + "원";
+                    break;
+                case "3":
+                    price = 2300 * count;
+                    sum += price;
+                    order += "참치김밥: " + count + "개 - " + price + "원";
+                    break;
+                case "4":
+                    price = 3000 * count;
+                    sum += price;
+                    order += "그냥라면: " + count + "개 - " + price + "원";
+                    break;
+                case "5":
+                    price = 3500 * count;
+                    sum += price;
+                    order += "치즈라면: " + count + "개 - " + price + "원";
+                    break;
+                case "6":
+                    price = 4000 * count;
+                    sum += price;
+                    order += "짬뽕라면: " + count + "개 - " + price + "원";
+                    break;
+                case "7":
+                    price = 2500 * count;
+                    sum += price;
+                    order += "떡볶이: " + count + "개 - " + price + "원";
+                    break;
+                case "8":
+                    price = 2500 * count;
+                    sum += price;
+                    order += "순대: " + count + "개 - " + price + "원";
+                    break;
+                case "9":
+                    price = 1000 * count;
+                    sum += price;
+                    order += "오뎅: " + count + "개 - " + price + "원";
+                    break;
+                case "10":
+                    price = 1000 * count;
+                    sum += price;
+                    order += "음료수: " + count + "개 - " + price + "원";
+                    break;
+                default:
+                    System.out.println("잘못누르셨습니다.");
+                    break;
+            }
+
+            System.out.print("추가주문하시겠습니까?(y/n) => ");
+            moreOrder = sc.next().toLowerCase().charAt(0);
+
+            // 개행문자추가
+            if (moreOrder == 'y')
+                order += "\n";
+
+        } while (moreOrder == 'y');
+
+        System.out.println("주문하신 정보는 다음과 같습니다.\n----------------------------");
+        System.out.println(order);
+        System.out.println("----------------------------");
+        System.out.println("합계 : " + sum + "원");
 
     }
 
-    private static void finishMenu() {
-
-    }
-
-    private static void selectMenu() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("원하시는 메뉴의 번호를 입력하세요 : ");
-        int num = sc.nextInt();
-        System.out.print("수량을 입력하세요 : ");
-        int count = sc.nextInt();
-    }
-
-    private static void showMenu() {
-        System.out.println("김밥류==============");
-        System.out.println("1.원조김밥=====1500원");
-        System.out.println("2.치즈김밥=====2000원");
-        System.out.println("3.참치김밥=====2300원");
-        System.out.println("라면류==============");
-        System.out.println("4.그냥라면=====3000원");
-        System.out.println("5.치즈라면=====3500원");
-        System.out.println("6.짬뽕라면=====4000원");
-        System.out.println("분식류==============");
-        System.out.println("7.떡볶이=======2500원");
-        System.out.println("8.순대========2500원");
-        System.out.println("9.오뎅========1000원");
-        System.out.println("기타================");
-        System.out.println("10.음료수======1000원");
-        System.out.println("\n---------------------------");
-    }
 }
