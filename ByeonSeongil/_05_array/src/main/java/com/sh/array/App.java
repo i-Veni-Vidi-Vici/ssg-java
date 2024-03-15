86ab6c03c01f2ef3fcdd5173bc366e46b6ac4dab
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-15 12:10:53 +0900

    feat(array): create how to use array of base level - Array1

---
+++ ByeonSeongil/_05_array/src/main/java/com/sh/array/App.java
@@ -0,0 +1,13 @@
+package com.sh.array;
+
+public class App {
+    public static void main(String[] args) {
+        Array1 array1 = new Array1();
+//        array1.test();
+//        array1.test2();
+//        array1.test3();
+//        array1.test4();
+        Array2 array2 = new Array2();
+        array2.test1();
+    }
+}
