e603a1e583aadfb21fd74ff3187ce1364da51ac7
nnieun <abocado216@naver.com>
2024-03-17 16:47:58 +0900

    Feat : 반복문 실습문제-문제6,7

--- KimNakyoung/_04_control_flow/src/main/java/com/ssg/test/loop/Test6.java
+++ KimNakyoung/_04_control_flow/src/main/java/com/ssg/test/loop/Test6.java
@@ -13,51 +13,14 @@ import java.util.Scanner;
 public class Test6 {
     public static void main(String[] args) {
 
-        System.out.println("김밥류 ===================\n" +
-                "1.원조김밥===========1500원\n" +
-                "2.치즈김밥===========2000원\n" +
-                "3.참치김밥===========2300원\n" +
-                "라면류 ===================\n" +
-                "4.그냥라면===========3000원\n" +
-                "5.치즈라면===========3500원\n" +
-                "6.짬뽕라면===========4000원\n" +
-                "분식류 ===================\n" +
-                "7.떡볶이=============2500원\n" +
-                "8.순대==============2500원\n" +
-                "9.오뎅==============1000원\n" +
-                "기타 ====================\n" +
-                "10.음료수===========1000원\n");
-
-
         Scanner sc = new Scanner(System.in);
 
-//        if(num == 1) {
-//            System.out.println("메뉴선택");
-//        }
-//        if(num == 2) {
-//            System.out.println("메뉴선택");
-//        }
-//        if(num == 3) {
-//            System.out.println("메뉴선택");
-//        }
-//        if(yn == 'y' ) {
-//            num = 1 ;
-//        }
 
-        String menu = ""; // 메뉴
-        int menuNum = 0; // 메뉴 개수
-        int cnt = 0; // 개수
-        String money = ""; //가격
 
-//        System.out.println(
-//                "1. 메뉴선택\n" +
-//                        "2. 수량선택\n" +
-//                        "3. 추가주문여부선택(y/n)\n");
-//
-//        System.out.println("어디로 가시겠습니까? ");
-//        int choice = sc.nextInt();
+        int totalCost = 0; // 총 비용
+        String order = " "; // 주문목록
 
-        for (; ; ) {
+        do {
 
                 System.out.println("김밥류 ===================\n" +
                         "1.원조김밥===========1500원\n" +
@@ -76,57 +39,88 @@ public class Test6 {
 
                 System.out.println("");
                 System.out.print("메뉴선택 : ");
-                int num = sc.nextInt();
+                int option = sc.nextInt();
 
-                switch (num) {
-                    case 1:
-                        menu = "원조김밥";
-                        money = "1500";
-                    case 2:
-                        menu = "치즈김밥";
-                        money = "2000";
-                    case 3:
-                        menu = "참치김밥";
-                        money = "2300";
-                    case 4:
-                        menu = "그냥라면";
-                        money = "3000";
-                    case 5:
-                        menu = "치즈라면";
-                        money = "3500";
-                    case 6:
-                        menu = "짬뽕라면";
-                        money = "4000";
-                    case 7:
-                        menu = "떡볶이";
-                        money = "2500";
-                    case 8:
-                        menu = "순대";
-                        money = "2500";
-                    case 9:
-                        menu = "오뎅";
-                        money = "1000";
-                    case 10:
-                        menu = "음료수";
-                        money = "1000";
-                }
+                String product = ""; // 메뉴
+
 
                 System.out.print("수량선택 : ");
-                menuNum = sc.nextInt(); // 수량
+                int quantity = sc.nextInt(); // 수량
+                int price = 0; //메뉴가격
+
+
+                switch (option) {
+                    case 1:
+                        price = 1500 * quantity;
+                        totalCost += price;
+                        order += "원조김밥: " + quantity + "개" + price + "원";
+                        break;
+                    case 2:
+                        price = 2000 * quantity;
+                        totalCost += price;
+                        order += "치즈김밥: " + quantity + "개" + price + "원";
+                        break;
+
+                    case 3:
+                        price = 2300 * quantity;
+                        totalCost += price;
+                        order += "참치김밥: " + quantity + "개" + price + "원";
+                        break;
+                    case 4:
+                        price = 3000 * quantity;
+                        totalCost += price;
+                        order += "그냥김밥: " + quantity + "개" + price + "원";
+                        break;
+                    case 5:
+                        price = 3500 * quantity;
+                        totalCost += price;
+                        order += "치즈라면: " + quantity + "개" + price + "원";
+                        break;
+
+                    case 6:
+                        price = 4000 * quantity;
+                        totalCost += price;
+                        order += "짬뽕라면: " + quantity + "개" + price + "원";
+                        break;
+                    case 7:
+                        price = 2500 * quantity;
+                        totalCost += price;
+                        order += "떡볶이: " + quantity + "개" + price + "원";
+                        break;
+                    case 8:
+                        price = 2500 * quantity;
+                        totalCost += price;
+                        order += "순대: " + quantity + "개" + price + "원";
+                        break;
+                    case 9:
+                        price = 1000 * quantity;
+                        totalCost += price;
+                        order += "오뎅: " + quantity + "개" + price + "원";
+                        break;
+                    case 10:
+                        price = 1000 * quantity;
+                        totalCost += price;
+                        order += "음료수: " + quantity + "개" + price + "원";
+
+                }
 
                 System.out.println("추가 주문 하시겠습니까(y/n)?");
-                int yN = sc.next().charAt(0);
-
+                char yN = sc.next().charAt(0);
                 if (yN == 'y') {
-                    // 1 메뉴선택으로 돌아가기
+                    order += "\n";
+                    continue;
                 }
 
                 if (yN == 'n') {
-                    System.out.println("주문하신 정보는 다음과 같습니다.");
-                    System.out.println("============================");
-                    break;
+                    break; // 반복문 탈출
                 }
-            }
+            }while (true);
+
+        System.out.println("주문하신 정보는 다음과 같습니다.");
+        System.out.println("====================================");
+        System.out.println(order);
+        System.out.println("====================================");
+        System.out.println("합계 : " + totalCost + "원");
 
         }
 
