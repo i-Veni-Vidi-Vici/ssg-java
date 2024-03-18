e1b02aa6b5c6b0e2c806fa89d4e0e072806c25d6
nnieun <abocado216@naver.com>
2024-03-18 23:49:06 +0900

    Feat : 자바 기초 프로그래밍 심화 실습문제

--- KimNakyoung/_05_array/src/main/java/com/ssg/test/multi/dimention/array/MultiDimentionalArray.java
+++ KimNakyoung/_05_array/src/main/java/com/ssg/test/multi/dimention/array/MultiDimentionalArray.java
@@ -10,24 +10,57 @@ public class MultiDimentionalArray {
 
     public static void main (String[] args) {
         MultiDimentionalArray multiDimentionalArray = new MultiDimentionalArray();
-        multiDimentionalArray.test1();
+//        multiDimentionalArray.test1();
+//        multiDimentionalArray.test2();
+//        multiDimentionalArray.test3();
+        multiDimentionalArray.test4();
     }
 
-//    문제 1 . 2차원 배열 int[4][4] 에 중첩 반복문을 사용해서 값대입 후 출력 하세요.
 
-    public void test1(){
+
+    // 문제 2
+    public void test2(){
         int[][] arr = new int[4][4];
 
+        //행 0123 열 0 / 행 0123 열 1 / 행 0123 열 2 / 행 0123 열 3 이라 자리 바꿔줌
         int k = 1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = k++;
+                arr[j][i] = k++;
 
             }
         }print(arr);
 
     }
 
+    // 문제 3
+    public void test3() {
+        int[][] arr = new int[4][4];
+
+        // 행 0123 열 3210
+        int k =1;
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = arr.length-1 ;  j >= 0 ; j-- ){
+                arr[i][j] = k++;
+            }
+
+        }print(arr);
+    }
+
+    //문제 4
+
+    public void test4() {
+        int[][] arr = new int[4][4];
+
+        // 행 0123 동안 열 3 고정 / 2 / 1 이라서 둘이 바꿔줌
+        int k = 1;
+        for (int i = arr.length-1 ;  i >=0 ; i--) {
+            for (int j = arr.length-1; j >=0 ; j--) {
+                arr[j][i] = k++;
+            }
+
+        }print(arr);
+    }
 
     // 출력메소드
     public void print(int[][] arr){
