337f64b4b7c1785bb6a229dc1449e66567353810
Hong-ji-min <jmh907@naver.com>
2024-03-18 17:52:05 +0900

    Feat : 중첩 반복문 & 다차원 배열 실습 문제 푸는 중

---
+++ HongJimin/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/App.java
@@ -0,0 +1,13 @@
+package com.ssg.test.multi.dimentional.array;
+
+public class App {
+    public static void main(String[] args) {
+        MultiDimentionalArray multiDimentionalArray = new MultiDimentionalArray();
+//        multiDimentionalArray.test1();
+//        multiDimentionalArray.test2();
+        multiDimentionalArray.test3();
+//        multiDimentionalArray.test4();
+//        multiDimentionalArray.test5();
+//        multiDimentionalArray.test6();
+    }
+}
