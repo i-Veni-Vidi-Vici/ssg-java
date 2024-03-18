fc854e38e832493c40dee8b2f1994891ed92adba
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-18 16:43:21 +0900

    feat(Sample): create 다차원 배열 실습문제2 - exercise2()

--- ByeonSeongil/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/Sample.java
+++ ByeonSeongil/_05_array/src/main/java/com/ssg/test/multi/dimentional/array/test/Sample.java
@@ -13,11 +13,46 @@ public class Sample {
                 numbers[i][j] = random.nextInt(100) + 1;
 
                 if (numbers[i][j] % 5 == 0) {
-                    System.out.printf("%d (%d, %d)\n",numbers[i][j], i,j);
+                    System.out.printf("%d (%d, %d)\n", numbers[i][j], i, j);
                 }
             }
         }
     }
 
+    public void exercise2() {
+        int[][] numbers = new int[4][4];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (i <= 2 && j <= 2) {
+                    numbers[i][j] = random.nextInt(100) + 1;
+                    rowSum += numbers[i][j];
+                } else {
+                    numbers[i][j] = rowSum;
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int colSum = 0;
+
+            for (int j = 0; j < 4; j++) {
+                if (i <= 2 && j <= 2) {
+                    colSum += numbers[j][i];
+                } else {
+                    numbers[j][i] = colSum;
+                }
+            }
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.printf("%d ", numbers[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
 
