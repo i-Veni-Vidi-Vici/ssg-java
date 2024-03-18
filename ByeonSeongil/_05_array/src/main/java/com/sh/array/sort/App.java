11aeb262956b44f2b23e101f12252e872ccd03b2
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-18 14:15:10 +0900

    feat(array.sort): create how to use sequentialsort

---
+++ ByeonSeongil/_05_array/src/main/java/com/sh/array/sort/App.java
@@ -0,0 +1,15 @@
+package com.sh.array.sort;
+
+public class App {
+    public static void main(String[] args) {
+        Swap swap = new Swap();
+        swap.test1();
+
+        // 순차 정렬
+        SequentialSort sequentialSort = new SequentialSort();
+        sequentialSort.test();
+        // 버블 정렬
+
+        // 삽입 정렬
+    }
+}
