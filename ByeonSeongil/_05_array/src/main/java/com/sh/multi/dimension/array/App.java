17c6f533cc4e9a5cf755028935430d5e90788176
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-19 09:20:01 +0900

    feat(dimension.array): create 중첩반복문 & 다차원 배열 문제1

---
+++ ByeonSeongil/_05_array/src/main/java/com/sh/multi/dimension/array/App.java
@@ -0,0 +1,8 @@
+package com.sh.multi.dimension.array;
+
+public class App {
+    public static void main(String[] args) {
+        Problem1 problem1 = new Problem1();
+        problem1.test();
+    }
+}
---
+++ ByeonSeongil/_05_array/src/main/java/com/sh/multi/dimension/array/Problem1.java
@@ -0,0 +1,18 @@
+package com.sh.multi.dimension.array;
+
+public class Problem1 {
+    public void test() {
+        int[][] numbers = new int[4][4];
+        MultiDimensionalArray multiDimensionalArray = new MultiDimensionalArray();
+
+        int count =1;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                numbers[i][j] = count++;
+            }
+        }
+
+        multiDimensionalArray.print(numbers);
+
+    }
+}
