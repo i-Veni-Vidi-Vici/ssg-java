63282432b2301238ff04db2ffc345e4ef117f53d
yongjun98 <dldydwns2020@naver.com>
2024-03-19 16:37:31 +0900

    Feat : 클래스/객체/캡슐화/생성자

---
+++ LeeYongjun/_05_array/src/main/java/com/ssg/test/array/Test3.java
@@ -0,0 +1,14 @@
+package com.ssg.test.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Test3 test3 = new Test3();
+        test3.test3();
+    }
+
+    private void test3() {
+    }
+}
