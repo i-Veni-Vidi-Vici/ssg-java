807fd039d6a6a43b169f9f976f61493887a3ca3c
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-03-19 09:22:41 +0900

    Fix: 자바 기초 프로그래밍 실습 문제 수정

--- JeongYejin/_05_array/src/main/java/com/ssg/test/lotto/LottoApp.java
+++ JeongYejin/_05_array/src/main/java/com/ssg/test/lotto/LottoApp.java
@@ -13,7 +13,8 @@ public class LottoApp {
                 lotto[i] = num;
             }
         }
-        ascending(lotto);
+//        ascending(lotto);
+        Arrays.sort(lotto);
         print(lotto);
     }
 
@@ -48,3 +49,36 @@ public class LottoApp {
         }
     }
 }
+
+//package com.ssg.test.lotto;
+//
+//import java.util.Arrays;
+//
+//public class LottoApp {
+//    public static void main(String[] args) {
+//        int[] lotto = new int[6];
+//        int index = 0;
+//
+//        // 6개의 중복없는 난수를 뽑기 위해 몇번을 반복할지 알수 없어 무한반복 처리한다.
+//        abc:
+//        while (true) {
+//            int n = (int) (Math.random() * 45) + 1;
+//            // 이미 뽑힌 숫자와 중복여부 검사
+//            // 중복되었다면 abc반복문 처음으로 가서 다시 뽑는다.
+//            for (int j = 0; j < index; j++) { // index 0일 때는 중복검사가 필요없기 때문에 생략됨
+//                if (n == lotto[j]) {
+//                    continue abc;
+//                }
+//            }
+//            // 중복되지 않았다면, lotto 배열에 추가한다.
+//            lotto[index++] = n;
+//            // lotto[0] = n으로 대입 후 index = 1이 되므로 중복검사 for문 실행!
+//            // 중복없이 6개를 모두 뽑은 경우 반복 중지한다.
+//            if (index == lotto.length)
+//                break;
+//        }
+//
+//        Arrays.sort(lotto); // Arrays가 제공하는 sort메소드
+//        System.out.println(Arrays.toString(lotto));
+//    }
+//}
