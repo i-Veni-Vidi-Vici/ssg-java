4d61eec8123651f59765511f6c36d956a38c91f9
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-19 09:19:01 +0900

    feat(dimesion.array): create print(array) - MultiDimensionalArray

---
+++ ByeonSeongil/_05_array/src/main/java/com/sh/multi/dimension/array/MultiDimensionalArray.java
@@ -0,0 +1,14 @@
+package com.sh.multi.dimension.array;
+
+public class MultiDimensionalArray {
+    public void print(int[][] arr){
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
