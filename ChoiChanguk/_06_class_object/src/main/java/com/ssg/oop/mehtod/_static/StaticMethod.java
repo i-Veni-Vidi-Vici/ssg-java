3a37f2448592d4d7996d1ff44a9acfe4045da563
cstangga <cstangga92@gmail.com>
2024-03-20 20:12:28 +0900

    Feat : 클래스/객체/오버로딩/call by/싱클톤/final/실습문제

--- ChoiChanguk/_06_class_object/src/main/java/com/ssg/oop/mehtod/_static/StaticMethod.java
+++ ChoiChanguk/_06_class_object/src/main/java/com/ssg/oop/mehtod/_static/StaticMethod.java
@@ -1,34 +1,46 @@
 package com.ssg.oop.mehtod._static;
 
+import java.util.Arrays;
+import java.util.SimpleTimeZone;
+
 public class StaticMethod {
     public static String toUpper(String str)
     {
-        String str1;
         return str.toUpperCase();
     }
 
     public static String setChar(String str, int num, char ch)
     {
         //전달 받은 문자열을 number째 있는 문자를 ch로 바꿔라
+        char[] temp=new char[str.length()];
         for(int i=0;i<str.length();i++)
         {
-            //str+i=num;
+            if(i==num)
+            {
+                temp[i]+= ch;
+            }
+            else
+                temp[i] += str.charAt(i);
         }
-        return str;
+        return String.valueOf(temp);
     }
 
     //전달 받은 문자열에서 영문자 개수를 리턴
     public static int getAlphabetLenght(String str)
     {
         int cnt=0;
+        for (int i=0;i<str.length();i++)
+        {
+            if((str.charAt(i)>='a'&&str.charAt(i)<='z')||
+                    str.charAt(i)>='A'&&str.charAt(i)<='Z')
+                cnt++;
+        }
         return cnt;
     }
 
     //전달받은 문자열값을 하나로 합쳐서 리턴
     public static String concat(String str1,String str2)
     {
-        String sumStr;
-        sumStr=str1.concat(str2);
-        return sumStr;
+        return str1+str2;
     }
 }
