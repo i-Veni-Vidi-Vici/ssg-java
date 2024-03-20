ba41bf0b9b265f167d937372c31f7c2c856ae4ad
Keryzy <ycseung00@gmail.com>
2024-03-20 16:35:34 +0900

    Feat: 클래스/객체/오버로딩/call by/싱글톤/final

---
+++ SeungYoonchan/_06_class_object/src/main/java/com/sh/singletone/Manager.java
@@ -0,0 +1,23 @@
+package com.sh.singletone;
+
+public class Manager {
+    private static Manager instance;
+    private String[] values = new String[3];
+
+    /**
+     * private 생성자를 통해 클래스외부에서 객체 생성 방지
+     */
+    private Manager(){}
+
+    public static Manager getInstance(){
+        // 최초 1회만 실행
+        if(instance == null) {
+            instance = new Manager();
+        }
+        return instance;
+    }
+
+    public String[] getValues(){
+        return this.values;
+    }
+}
