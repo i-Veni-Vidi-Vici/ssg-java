46d78fdac54828b619c9b8fe8906715f1b8ec42e
subkka <beeniaa@naver.com>
2024-03-21 16:03:12 +0900

    Update: static 메소드 실습문제 제출

--- ParkSubeen/_06_class_object/src/main/java/com/ssg/oop/method/_static/Run.java
+++ ParkSubeen/_06_class_object/src/main/java/com/ssg/oop/method/_static/Run.java
@@ -3,12 +3,28 @@ package com.ssg.oop.method._static;
 public class Run {
     public static void main(String[] args) {
         //1 toUpper메소드 호출시 "ssg academy"를 전달하면, "SSG ACADEMY"가 리턴
+//        StaticMethod staticMethod1 = new StaticMethod();
+//        String toUpperMethod = staticMethod1.toUpper("ssg academy");
+//        System.out.println(toUpperMethod);
+        System.out.println(StaticMethod.toUpper("ssg academy"));
 
         //2 setChar메소드 호출시  "java", 0, 'x'를 전달하고, "xava" 리턴
+//        StaticMethod staticMethod2 = new StaticMethod();
+//        String setCharMethod = staticMethod2.setChar("java", 0, 'x');
+//        System.out.println(setCharMethod);
+        System.out.println(StaticMethod.setChar("java", 0, 'x'));
 
         //3 getAlphabetLength호출시 "안녕, 난 James야!" 전달하고, 5 리턴
+//        StaticMethod staticMethod3 = new StaticMethod();
+//        int cnt = staticMethod3.getAlphabetLength("안녕, 난 James야!");
+//        System.out.println(cnt);
+        System.out.println(StaticMethod.getAlphabetLength("안녕, 난 James야!"));
 
         //4 concat 호출시 "hello", "world" 호출후 "helloworld" 리턴
+//        StaticMethod staticMethod4 = new StaticMethod();
+//        String concatValue = staticMethod4.concat("hello", "world");
+//        System.out.println(concatValue);
+        System.out.println(StaticMethod.concat("hello", "world"));
 
     }
 }
--- ParkSubeen/_06_class_object/src/main/java/com/ssg/oop/method/_static/StaticMethod.java
+++ ParkSubeen/_06_class_object/src/main/java/com/ssg/oop/method/_static/StaticMethod.java
@@ -5,21 +5,35 @@ public class StaticMethod {
     //1. 전달한 문자열을 모두 대문자로 바꾸는 static 메소드
     //메소드명 : toUpper(String) : String
     public static String toUpper(String str) {
-        return null;
+        return str.toUpperCase();
     }
 
 
     //2. 문자열(1)에서 전달받은 인덱스(2)의 문자를 전달받은 문자(3)로 변경하는 static 메소드
     //메소드명 : setChar(String, int, char) : String
     public static String setChar(String str, int n, char change) {
-        return null;
+        char[] arr = str.toCharArray();
+        arr[n] = change;
+        return new String(arr);
     }
 
 
     //3. 전달한 문자열에서 영문자의 개수를 리턴하는 static 메소드
     //메소드명 : getAlphabetLength(String) : int
+    public static int getAlphabetLength(String str) {
+        int cnt = 0;
+        for(int num = 0; num < str.length(); num++) {
+            if(str.charAt(num) >= 'A' && str.charAt(num) < 'Z' || str.charAt(num) >= 'a' && str.charAt(num) < 'z') {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 
 
     //4. 전달한 문자열값을 하나로 합쳐서 리턴
     //메소드명 : concat(String, String) : String
+    public static String concat(String str1, String str2) {
+        return str1 + str2;
+    }
 }
