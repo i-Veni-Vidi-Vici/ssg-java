fd5949a3f45db3177ec0d5c16f2f895f377b2c64
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-03-22 18:21:31 +0900

    Feat: 포인트관리시스템 실습문제2 추가

--- JeongYejin/_07_object_array/src/main/java/com/ssg/member/model/Gold.java
+++ JeongYejin/_07_object_array/src/main/java/com/ssg/member/model/Gold.java
@@ -1,41 +1,12 @@
 package com.ssg.member.model;
 
-public class Gold {
-    private String name;
-    private String grade;
-    private int point;
+public class Gold extends Member {
 
     public Gold(String name, String grade, int point) {
-        this.name = name;
-        this.grade = grade;
-        this.point = point;
+        super(name, grade, point);
     }
 
     public double getEjapoint() {
         return this.getPoint() * 0.05;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    public int getPoint() {
-        return point;
-    }
-
-    public void setPoint(int point) {
-        this.point = point;
     }
 }
--- JeongYejin/_07_object_array/src/main/java/com/ssg/member/model/Vip.java
+++ JeongYejin/_07_object_array/src/main/java/com/ssg/member/model/Vip.java
@@ -1,42 +1,12 @@
 package com.ssg.member.model;
 
-public class Vip {
-
-    private String name;
-    private String grade;
-    private int point;
+public class Vip extends Member {
 
     public Vip(String name, String grade, int point) {
-        this.name = name;
-        this.grade = grade;
-        this.point = point;
+        super(name, grade, point);
     }
 
     public double getEjapoint() {
         return this.getPoint() * 0.1;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    public int getPoint() {
-        return point;
-    }
-
-    public void setPoint(int point) {
-        this.point = point;
     }
 }
