83d1c7b2ba483f94c2ebdbf1b1f79ae283425095
youngwoo2 <leeduddn98@gmail.com>
2024-03-22 09:18:32 +0900

    Feat: 클래스/객체실습문제2

--- LeeYoungwoo/_06_class_object/src/main/java/com/ssg/oop/emp/run/EmpTest.java
+++ LeeYoungwoo/_06_class_object/src/main/java/com/ssg/oop/emp/run/EmpTest.java
@@ -21,23 +21,24 @@ public class EmpTest {
                 *********************************
                 입력 : """;
 
-        Employee e = new Employee();
+        Employee e = null;
         while (true) {
             System.out.print(menu);
             String choice = sc.next();
             // 사원정보를 입력하지 않고, 삭제/출력할 수 없다 --> 이거만 해결하면됨!
             switch (choice) {
-                case "1" : e.empInput(); break;
+                case "1" : e = new Employee();  e.empInput(); break;
                 case "2" : if(e == null) {
-                    System.out.println("> 먼저 사원정보를 입력해주세요...");
+                    System.out.println("> 먼저 회원정보를 입력해주세요...");
                     break;
                 }
-                e = null; break;
+                    e = null;
+                    System.out.println("> 회원정보를 삭제하였습니다..."); break;
                 case "3" : if(e == null) {
-                    System.out.println("> 먼저 사원정보를 입력해주세요...");
+                    System.out.println("> 먼저 회원정보를 입력해주세요...");
                     break;
                 }
-                e.empOutput(); break;
+                    e = new Employee(); e.empOutput(); break;
                 case "0" : return ;
                 default:
                     System.out.println("> 잘못 입력하셨습니다...");
