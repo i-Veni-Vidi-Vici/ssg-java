2a52e09e4323fa66ced6ae3c3821a37287bfd308
vlshzl35 <>
2024-03-24 23:57:55 +0900

    Update: 06 variable 디렉토리 내용추가

--- YangHeeyun/_06_class_object/src/main/java/com/sh/variable/App.java
+++ YangHeeyun/_06_class_object/src/main/java/com/sh/variable/App.java
@@ -7,6 +7,7 @@ package com.sh.variable;
  *        - 클래스 변수(static 변수) 해당클래스 처음 사용된 시점 ~ 프로그램 종료
  *        - 인스턴스 변수(non-static변수, 멤버변수) new연산자로 객체 생성시~ 객체 제거
  * 2. 지역변수 : 메소드 영역에서 선언된 변수. 메소드호출~메소드 리턴
+ *        - 매개변수도 하나의 지역변수다.
  * 짧은 생명주기를 가질수록 메모리 효율은 좋아진다.
  * </pre>
  */
@@ -14,7 +15,7 @@ public class App {
     private static int snum; // 클래스 변수
     private int num; // 인스턴스 변수
 
-    public void test(int m){
+    public void test(int m){ // 매개변수 m도 지역변수
         int k;// 지역변수
     }
 }
