fbb0fcbcb22bf73845fda0da1eda78e3e28a02c8
vlshzl35 <>
2024-03-24 23:16:45 +0900

    필드 실습문제 풀이

--- YangHeeyun/_06_class_object/src/main/java/com/ssg/oop/field/Test.java
+++ YangHeeyun/_06_class_object/src/main/java/com/ssg/oop/field/Test.java
@@ -11,7 +11,7 @@ public class Test {
 
         Test t3 = new Test();
         t.test3(t3);
-        System.out.println(t3.a);
+        System.out.println(t3.a); //10
 
         t.test4(t);
     }
@@ -19,25 +19,26 @@ public class Test {
     public void test1() {
         int a = 1000;
         int s = 999;
-        System.out.println(this.a); //
-        System.out.println(Test.s);
+        System.out.println(this.a); //100
+        System.out.println(Test.s); //99
     }
 
     public void test2() {
         Test t = new Test();
         t.a = 1000;
-        System.out.println(a);
+        System.out.println(a); //100
+        // this.a로 전역변수 a값을 읽음
     }
 
     public void test3(Test t) {
         t.a = 10;
         this.a = 200;
-        System.out.println(t.a);
+        System.out.println(t.a); // 10
     }
 
     public void test4(Test t) {
         t.a = 1000;
-        System.out.println(this.a);
+        System.out.println(this.a);//1000
     }
 }
 
