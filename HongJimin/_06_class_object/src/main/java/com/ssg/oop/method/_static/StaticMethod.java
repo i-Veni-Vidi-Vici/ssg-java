57fb422eb99f055a67ac74d7d584889c7c0434da
Hongjimin <jmh907@naver.com>
2024-03-25 22:22:19 +0900

    수정사항

---
+++ HongJimin/_06_class_object/src/main/java/com/ssg/oop/method/_static/StaticMethod.java
@@ -0,0 +1,20 @@
+package com.ssg.oop.method._static;
+
+public class StaticMethod {
+
+    //static method
+    //1. 전달한 문자열을 모두 대문자로 바꾸는 static 메소드
+    //메소드명 : toUpper(String) : String
+
+
+    //2. 문자열(1)에서 전달받은 인덱스(2)의 문자를 전달받은 문자(3)로 변경하는 static 메소드
+    //메소드명 : setChar(String, int, char) : String
+
+
+    //3. 전달한 문자열에서 영문자의 개수를 리턴하는 static 메소드
+    //메소드명 : getAlphabetLength(String) : int
+
+
+    //4. 전달한 문자열값을 하나로 합쳐서 리턴
+    //메소드명 : concat(String, String) : String
+}
