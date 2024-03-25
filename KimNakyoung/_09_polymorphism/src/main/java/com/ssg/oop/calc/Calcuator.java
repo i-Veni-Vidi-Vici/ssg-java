3b8c12df363755856efc27f8c6d6e38efdc7c511
nnieun <abocado216@naver.com>
2024-03-25 18:16:02 +0900

    Feat: 다형성실습문제

---
+++ KimNakyoung/_09_polymorphism/src/main/java/com/ssg/oop/calc/Calcuator.java
@@ -0,0 +1,15 @@
+package com.ssg.oop.calc;
+
+/**
+ * 부모클래스
+ *
+ */
+public abstract class Calcuator {
+    private int num1;
+    private int num2;
+
+    //추상클래스
+    public abstract void calc();
+
+
+}
