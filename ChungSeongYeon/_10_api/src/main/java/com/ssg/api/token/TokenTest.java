1427b736347775f6b30f8d322ed9880405b4a410
seongyeongit <wjd12118@nate.com>
2024-03-26 17:44:56 +0900

    Feat: 10과제 1,2 & 3(추가예정)

---
+++ ChungSeongYeon/_10_api/src/main/java/com/ssg/api/token/TokenTest.java
@@ -0,0 +1,14 @@
+package com.ssg.api.token;
+
+import java.util.StringTokenizer;
+
+public class TokenTest {
+    public static void main(String[] args) {
+        String str = "J a v a P r o g r a m";
+        StringTokenizer stokenizer = new StringTokenizer(str, " ");
+        char[] result = new char[stokenizer.countTokens()];
+        int index = 0;
+
+        //+++ 추가 작성해야 함.
+    }
+}
