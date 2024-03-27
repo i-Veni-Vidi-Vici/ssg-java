5287cc1403367c5ddc9b6c2f44681982487fff9d
vlshzl35 <>
2024-03-27 23:02:00 +0900

    상속 실습문제1 풀이

--- YangHeeyun/_08_inheritance/src/main/java/com/hw1/run/Employee.java
+++ YangHeeyun/_08_inheritance/src/main/java/com/hw1/run/Employee.java
@@ -1,47 +1,21 @@
 package com.hw1.run;
 
-public class Employee {
+public class Employee extends Person{
     private int salary; // 급여
-    private String dept; // 부서\
+    private String dept; // 부서
 
-    private String name;
-    private int age;
-    private double height;
-    private double weight;
+    Person person = new Person();
 
-    public Employee() {
+    public Employee() {}
+    public Employee(String name, int age, double height, double weight, int salary, String dept) {
+        super(age, height, weight);
+        person.setName(name);
+        this.salary = salary;
+        this.dept = dept;
     }
 
-    public Employee(int age, double height, double weight) {
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-    }
-
+    @Override
     public String information(){
-
-    }
-    public int getAge(){
-        return age;
-    }
-
-    public void setAge(int age){
-        this.age = age;
-    }
-
-    public double getHeight(){
-        return height;
-    }
-
-    public void setHeight(double height){
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight){
-        this.weight = weight;
+        return person.getName() + "  " + super.information() + "  " + salary + "  " + dept ;
     }
 }
