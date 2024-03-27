30e3f6ef9672329a7770828ec3533d30d4decf5e
CstoneKu <mskoo0725@gmail.com>
2024-03-27 18:32:16 +0900

    feet: api 과제2 구현중

---
+++ KuMinsang/_10_api/src/main/java/com/ssg/api/hair/model/dto/User.java
@@ -0,0 +1,106 @@
+package com.ssg.api.hair.model.dto;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private LocalDate birthday;
+    private LocalDate reservationDate;
+    private LocalDate reservationTime;
+    private LocalDate createAt;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName, String email, String gender, LocalDate birthday, LocalDate reservationDate, LocalDate reservationTime, LocalDate createAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.reservationDate = reservationDate;
+        this.reservationTime = reservationTime;
+        this.createAt = createAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public LocalDate getReservationTime() {
+        return reservationTime;
+    }
+
+    public void setReservationTime(LocalDate reservationTime) {
+        this.reservationTime = reservationTime;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public void information(){
+
+    }
+}
