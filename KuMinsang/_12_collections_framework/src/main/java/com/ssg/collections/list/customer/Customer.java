d2b31ba67b4d0088bae1911de5e9acb3b0cd7e4a
CstoneKu <CstoneKu@gmail.com>
2024-03-28 22:07:08 +0900

    feet : list 실습문제

--- KuMinsang/_12_collections_framework/src/main/java/com/ssg/collections/list/customer/Customer.java
+++ KuMinsang/_12_collections_framework/src/main/java/com/ssg/collections/list/customer/Customer.java
@@ -1,58 +1,55 @@
 package com.ssg.collections.list.customer;
 
 public class Customer {
-	private String name;
-	private int age;
-	private char gender;
-	private double point;
+    private String name;
+    private int age;
+    private char gender;
+    private double point;
 
-	public Customer() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public Customer() {
+    }
 
-	public Customer(String name, int age, char gender, double point) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
-		this.point = point;
-	}
+    public Customer(String name, int age, char gender, double point) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.point = point;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getAge() {
-		return age;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	public char getGender() {
-		return gender;
-	}
+    public char getGender() {
+        return gender;
+    }
 
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
 
-	public double getPoint() {
-		return point;
-	}
+    public double getPoint() {
+        return point;
+    }
 
-	public void setPoint(double point) {
-		this.point = point;
-	}
+    public void setPoint(double point) {
+        this.point = point;
+    }
 
-	@Override
-	public String toString() {
-		return "Customer [name=" + name + ", age=" + age + ", gender=" + gender + ", point=" + point + "]";
-	}
+    @Override
+    public String toString() {
+        return String.format("%s %d %c %.1f", name, age, gender, point);
+    }
 }
--- KuMinsang/_12_collections_framework/src/main/java/com/ssg/collections/list/customer/CustomerMain.java
+++ KuMinsang/_12_collections_framework/src/main/java/com/ssg/collections/list/customer/CustomerMain.java
@@ -1,46 +1,50 @@
 package com.ssg.collections.list.customer;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-
+import java.util.*;
 public class CustomerMain {
-	private ArrayList<Customer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        List<Customer> list = new ArrayList<Customer>();
+        list.add(new Customer("홍길동", 25, '남', 1250.5));
+        list.add(new Customer("박문수", 33, '남', 3457));
+        list.add(new Customer("김춘추", 38, '남', 2485.6));
+        list.add(new Customer("신사임당", 43, '여', 2300.9));
+        System.out.println(list);
 
-	public static void main(String[] args) {
-		CustomerMain main = new CustomerMain();
-		main.test1();
-		main.test2();
-		main.test3();
-		main.test4();
-	}
+        //리스트의 맨 앞에 새로운 데이터 추가
+        list.add(0, new Customer("이황", 66, '남', 9999.9));
+        System.out.println(list);
 
-	private void test1() {
-		this.list.add(new Customer("홍길동", 25, '남', 1250.5));
-		this.list.add(new Customer("박문수", 33, '남', 3457.8));
-		this.list.add(new Customer("김춘추", 38, '남', 2485.6));
-		this.list.add(new Customer("신사임당", 43, '여', 2300.9));
-		System.out.println("[문제1] " + this.list);
-	}
-	
-	private void test2() {
-		this.list.add(0, new Customer("이황", 66, '남', 9999.9));
-		System.out.println("[문제2] " + this.list);
-	}
-	
-	private void test3() {
-		// List#sort(Comparator) : Collections.sort(List, Comparator) 동일하다.
-		this.list.sort((c1, c2) -> c1.getName().compareTo(c2.getName()));
-		System.out.println("[문제3] " + this.list);
-	}
-	
-	private void test4() {
-		this.list.sort(new Comparator<Customer>() {
-			@Override
-			public int compare(Customer o1, Customer o2) {
-				return (int) (o2.getPoint() - o1.getPoint());
-			}
-		});
-		System.out.println("[문제4] " + this.list);		
-	}
+        //이름 키 값으로 하여 사전순 정렬
+        list.sort(new Comparator<Customer>() {
+            @Override
+            public int compare(Customer o1, Customer o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        System.out.println(list);
 
+        //나이 오름차순 정렬
+        list.sort(new Comparator<>(){
+            @Override
+            public int compare(Customer o1, Customer o2){
+                return o1.getAge()-o2.getAge();
+            }
+        });
+        System.out.println(list);
+
+        //포인트 내림차순
+        list.sort(new Comparator<>(){
+           @Override
+           public int compare(Customer o1, Customer o2){
+               return (int)(o2.getPoint()-o1.getPoint());   //비교 순서를 바꿔서 내림차순으로 동작
+           }
+        });
+        //Comparator 인터페이스의 compare는 비교 결과에 따라 양수, 음수, 0을 반환
+        //음수 : 첫 번째 인수가 두 번째 인수보다 작음
+        //양수 : 첫 번재 인수가 두 번째 인수보다 큼
+        // 0 : 두 인수가 같음
+        //이 규칙에 따르면 기본적인 로직은 오른차순으로 동작함
+        //반환 값의 부호를 바꿔주면 반대로 내림차순으로 동작함
+        System.out.println(list);
+
+    }
 }
