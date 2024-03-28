d35547f57fe7e1985938151603c9541492b7e844
CstoneKu <CstoneKu@gmail.com>
2024-03-28 21:29:00 +0900

    update: 수업자료 최신화

--- KuMinsang/_11_generics/src/main/java/com/sh/generics/_01/_class/Box.java
+++ KuMinsang/_11_generics/src/main/java/com/sh/generics/_01/_class/Box.java
@@ -2,11 +2,10 @@ package com.sh.generics._01._class;
 
 /**
  * 타입변수
- *  - T는 타입변수이다
- *  - T(type) , K(key), V(value), E(element) ....
- *  - 객체생성시 지정한 타입(클래스)가 타입변수에 바인딩된다
- *  - new Box<Integer> -> T= Integer
- *  - **컴파일타임**에 해당 타입으로 바인딩된다.
+ * - T(type), K(key), V(value), E(element) ...
+ * - 객체생성시 지정한 타입(클래스)가 타입변수에 바인딩 된다.
+ * - new Box<Integer> -> T = Integer
+ * - **컴파일타임**에 해당타입으로 바인딩된다.
  *
  * @param <T>
  */
@@ -20,4 +19,10 @@ public class Box<T> {
     public T getValue() {
         return this.value;
     }
+
+    public static <K> Box<K> of(K value) {
+        Box<K> box = new Box<>();
+        box.setValue(value);
+        return box;
+    } 
 }
--- KuMinsang/_11_generics/src/main/java/com/sh/generics/_02/_method/App.java
+++ KuMinsang/_11_generics/src/main/java/com/sh/generics/_02/_method/App.java
@@ -4,35 +4,33 @@ import com.sh.generics._01._class.Box;
 
 /**
  * <pre>
- *     제네릭 메소드
- *     - 타입변수를 메소드 레벨에서 선언하고 사용하는 것
- *
+ * 제네릭 메소드
+ * - 타입변수를 메소드레벨에서 선언하고 사용하는 것
  * </pre>
  */
 public class App {
     /**
-     * 맨앞의  <T>가 제네릭변수
-     * 두번째 T가 리턴 타입, 세번째는 매개변수이다
+     * 
      * @param arg
      * @return
-     * @param <T>
+     * @param <T> 타입변수
      */
-    public <T> T process(T arg){
-        //.... 타입별 분기 처리
+    public <T> T process(T arg) {
+        // ... 타입별 어쩌구 저쩌구
         return arg;
-    }
-
+    } 
+    
     public static void main(String[] args) {
         App app = new App();
         Integer result = app.process(123);
-        System.out.println(result instanceof Integer);  //true
+        System.out.println(result instanceof Integer); // true
 
         String result2 = app.process("안녕");
-        System.out.println(result2 instanceof String);  //true
+        System.out.println(result2 instanceof String); // true
 
-        //Box.<K>of(K value)
-        Box<Integer> box =Box.of(123);
+        // Box.<K>of(K value)
+        Box<Integer> box = Box.of(123);
         Integer value = box.getValue();
-
+        System.out.println(value instanceof Integer);
     }
 }
