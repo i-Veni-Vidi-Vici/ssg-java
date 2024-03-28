2a6d2fe13ab7856b8913e4d7992bbf40d470ed86
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-28 16:41:02 +0900

    feat(_11_generics): add learning about Generics - Generics 상한/하한 제한     - <A extends B>, <A super B> - Generics wildcard     - <? extends A>, <? super B>

---
+++ ByeonSeongil/_11_generics/src/main/java/com/sh/generics/_03/_extends/AnimalFarm.java
@@ -0,0 +1,12 @@
+package com.sh.generics._03._extends;
+
+/**
+ * <pre>
+ * <A implements 인터페이스>로 작성하지 않음
+ * 인턴페이스로 상한제한을 하는 경우 역시 <A extends 인터페이스>로 작성
+ * </pre>
+ * @param <A>
+ */
+public class AnimalFarm <A extends Animal>{
+
+}
