efc4a97cb8f5fa4d43ad814b478dd8c1e5368cce
CstoneKu <CstoneKu@gmail.com>
2024-03-30 21:14:43 +0900

    feet : collections실습문제2

--- KuMinsang/_12_collections_framework/src/main/java/com/ssg/collections/map/book/TestBookManager.java
+++ KuMinsang/_12_collections_framework/src/main/java/com/ssg/collections/map/book/TestBookManager.java
@@ -1,78 +1,74 @@
-package com.ssg.collections.list.book.run;
-
-import com.ssg.collections.list.book.controller.BookListManager;
+package com.ssg.collections.map.book;
 import com.ssg.collections.list.book.model.vo.Book;
+import com.ssg.collections.map.book.controller.BookMapManager;
 
+import java.util.Collections;
 import java.util.Scanner;
 
 public class TestBookManager {
-    private static BookListManager manager;
+    private static BookMapManager manager;
     private static Scanner sc;
+
     public static void main(String[] args) {
         TestBookManager testBookManager = new TestBookManager();
-        manager = new BookListManager();
-
+        manager = new BookMapManager();
         sc = new Scanner(System.in);
+
+
         manager.addBook(new Book("1", 1, "김춘추와연회", "박진"));
         manager.addBook(new Book("10", 3, "장수왕의복수", "핫하"));
         manager.addBook(new Book("6", 2, "고이왕의선택", "멍멍"));
         manager.addBook(new Book("8", 4, "장보고의대검", "애옹"));
         int choose;
         boolean flag = true;
-        while(flag){
+        while (flag) {
             testBookManager.menu();
-            int index=-1;
+            int index = -1;
             choose = sc.nextInt();
-            switch (choose){
-                case 1 :
+            switch (choose) {
+                case 1:
 //                1. 새 도서 추가
                     manager.addBook(testBookManager.inputBook());
                     break;
-                case 2 :
+                case 2:
 //                2. 도서정보 출력(책번호 오름차순)
                     System.out.println("도서정보를 번호 오름차순으로 출력");
-                    manager.printBookList(manager.sortedBookListByNo());
+                    manager.printBookMap(manager.SortedBookListByNo());
                     break;
-                case 3 :
+                case 3:
 //                3. 도서정보 출력(카테고리 오름차순)
                     System.out.println("도서정보를 카테고리 오름차순 출력");
-                    manager.printBookList(manager.sortedBookListByCategory());
+                    manager.printBookMap(manager.sortedBookListByCategory());
                     break;
-                case 4 :
+                case 4:
 //                4. 도서 삭제
                     System.out.print("삭제할 도서 이름을 입력하세요 : ");
-                    index = Integer.parseInt(testBookManager.inputBookTitle());
-                    if(index>=0)
-                        manager.deleteBook(index);
+                    manager.deleteBook(manager.searchBook(sc.next()));
                     break;
-                case 5 :
+                case 5:
 //                5. 도서 검색출력
                     System.out.println("검색할 도서명을 입력하세요");
-                    index = Integer.parseInt(testBookManager.inputBookTitle());
-                    if(index>=0)
-                        manager.printBook(index);
+                    manager.printBook(manager.searchBook(sc.next()));
                     break;
-                case 6 :
+                case 6:
 //                6. 전체 출력
                     manager.printAll();
                     break;
-                default :
-                    flag =false;
+                default:
+                    flag = false;
 //                0. 끝내기
             }
 
         }
     }
 
-    public String inputBookTitle(){
+    public String inputBookTitle() {
         //Index를 리턴한다
         String title = sc.next();
-        int index = manager.searchBook(title);
-//        sc.close();
-        return Integer.toString(index);
+        return manager.searchBook(title);
     }
 
-    public Book inputBook(){
+    public Book inputBook() {
         Book tmpBook = new Book();
         System.out.println("새로운 도서를 추가합니다.");
         System.out.println("도서 정보를 입력하세요.");
@@ -84,11 +80,10 @@ public class TestBookManager {
         tmpBook.setTitle(sc.next());
         System.out.print("저자 : ");
         tmpBook.setAuthor(sc.next());
-//        sc.close();
         return tmpBook;
     }
 
-    public void menu(){
+    public void menu() {
         String str = """
                 *** 도서 관리 프로그램 ***
                 1. 새 도서 추가
@@ -103,3 +98,4 @@ public class TestBookManager {
         System.out.println(str);
     }
 }
+
