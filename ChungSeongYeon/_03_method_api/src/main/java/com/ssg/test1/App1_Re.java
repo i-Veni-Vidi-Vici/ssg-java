14677769bab2c23ed99bd2e2708c1f88d691947a
seongyeongit <wjd12118@nate.com>
2024-04-01 00:30:46 +0900

    03_method 복습

---
+++ ChungSeongYeon/_03_method_api/src/main/java/com/ssg/test1/App1_Re.java
@@ -0,0 +1,8 @@
+package com.ssg.test1;
+
+public class App1Re {
+    public static void main(String[] args) {
+        RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
+        randomNumGenerator.generate();
+    }
+}
