a52dd5576196d6290d170850891bdbea2410c323
yongjun98 <dldydwns2020@naver.com>
2024-04-01 17:59:29 +0900

    Feat : 예외처리

---
+++ LeeYongjun/_13_exception_handling/src/main/java/com/sh/_04/checked/MemberAgeChecker.java
@@ -0,0 +1,27 @@
+package com.sh._04.checked;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MemberAgeChecker{
+
+    public static void checkAge() {
+        Scanner sc = new Scanner(System.in);
+        int age = 0;
+        do {
+            try {
+                System.out.println("나이 입력 : ");
+                age = sc.nextInt();
+            }
+            catch (InputMismatchException e){
+                System.out.println("유효한 숫자를 입력하세요...");
+                sc.next(); // 입력버퍼의 내용비우기 - 예외가 발생하면 버퍼의 내용을 제거하지 않는다.
+            }
+        }
+        while (age == 0);
+            if (age < 20) {
+                throw new NotValidMemberAgeException(String.valueOf(age));
+            }
+
+    }
+}
