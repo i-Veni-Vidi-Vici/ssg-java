e2517eefe2636bbf6d482511c9d69270fd78be08
vlshzl35 <>
2024-04-03 01:39:12 +0900

    16일차 정리 + object 실습문제 추가필요

--- YangHeeyun/_10_api/src/main/java/com/sh/api/_01/object/Book.java
+++ YangHeeyun/_10_api/src/main/java/com/sh/api/_01/object/Book.java
@@ -1,5 +1,7 @@
 package com.sh.api._01.object;
 
+import java.util.Objects;
+
 public class Book {
     private long id;
     private String title;
@@ -61,73 +63,84 @@ public class Book {
      * @param obj
      * @return
      */
-    @Override
-    public boolean equals(Object obj){
-        // 동일비교
-        if(this == obj)
-            return true;
-
-        // null비교
-        if(obj == null)
-            return false;
-
-        // 타입비교
-        if(!(obj instanceof Book))
-            return false;
-        // 필드비교
-        Book other = (Book) obj; // 다운캐스팅
-
-        // id필드(기본형) 비교
-        if(this.id != other.id)
-            return false;
-
-        // title필드(참조형) 비교
-        if(this.title == null){
-            // 현재객체의 title이 null인데, other객체의 title이 null이 아닌 경우
-            if(other.title != null)
-                return false;
-        }else{
-            // 오버라이드된 String#equals 대신 호출
-            if(!this.title.equals(other.title))
-                return false;
-        }
-
-        // author필드(참조형) 비교
-        if(this.author == null){
-            if(other.author != null){
-                return false;
-        }else{
-             if(!this.author.equals(other.author))
-                 return false;
-            }
-        }
-
-        // price필드(기본형) 비교
-        if(this.price != other.price)
-            return false;
-        return true;
-    }
+//    @Override
+//    public boolean equals(Object obj){
+//        // 동일비교
+//        if(this == obj) // 같은 객체라면 동일한 내용
+//            return true;
+//
+//        // null비교
+//        if(obj == null)
+//            return false;
+//
+//        // 타입비교
+//        if(!(obj instanceof Book))
+//            return false;
+//        // 필드비교
+//        Book other = (Book) obj; // 다운캐스팅
+//
+//        // id필드(기본형) 비교
+//        if(this.id != other.id)
+//            return false;
+//
+//        // title필드(참조형) 비교
+//        if(this.title == null){
+//            // 현재객체의 title이 null인데, other객체의 title이 null이 아닌 경우
+//            if(other.title != null)
+//                return false;
+//        }else{
+//            // 오버라이드된 String#equals 대신 호출
+//            if(!this.title.equals(other.title))
+//                return false;
+//        }
+//
+//        // author필드(참조형) 비교
+//        if(this.author == null){
+//            if(other.author != null){
+//                return false;
+//        }else{
+//             if(!this.author.equals(other.author))
+//                 return false;
+//            }
+//        }
+//
+//        // price필드(기본형) 비교
+//        if(this.price != other.price)
+//            return false;
+//        return true;
+//    }
 
     /**
      * 지정한 필드 기준으로 고유한 값을 만드는 연산
      * - 필드별로 특정수를 곱해 고유값 생성(31이란 소수를 관례적으로 사용)
      * @return
      */
-    @Override
-    public int hashCode(){
-        int result = 1; // 누적곱 연산의 준비값
-        final int PRIME = 31;
-        // id필드
-        result = result * PRIME + (int)id;
-        // title필드
-        if(title != null)
-            result = result * PRIME + title.hashCode();
-        // author필드
-        if(author != null)
-            result = result * PRIME + author.hashCode();
-        // price필드
-        result = result * PRIME + price;
+//    @Override
+//    public int hashCode(){
+//        int result = 1; // 누적곱 연산의 준비값
+//        final int PRIME = 31;
+//        // id필드
+//        result = result * PRIME + (int)id;
+//        // title필드
+//        result = result * PRIME + (title == null? 0:title.hashCode());
+//        // author필드
+//        result = result * PRIME + (author == null? 0: author.hashCode());
+//        // price필드
+//        result = result * PRIME + price;
+//
+//        return result;
+//    }
 
-        return result;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && price == book.price && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, price);
     }
 }
