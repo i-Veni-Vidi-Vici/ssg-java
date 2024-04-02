1f24013f048581688d4145d48eb991b52ec11439
seongyeongit <wjd12118@nate.com>
2024-04-02 16:29:03 +0900

    Feat: File API, 입출력 스트림 구분, 파일 입출력 작성, 보조스트림 적용

---
+++ ChungSeongYeon/_14_io/src/main/java/com/sh/io/_02/stream/App4.java
@@ -0,0 +1,27 @@
+package com.sh.io._02.stream;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * <pre>
+ * 글자 단위 출력스트림 FileWriter
+ * - char, String 타입의 데이터를 직접 쓰기 가능
+ *
+ * </pre>
+ */
+
+public class App4 {
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter(App.ROOT + "byeworld.txt")) {
+            fw.write("안녕\n");
+            fw.write("ㅋㅋㅋ\n");
+            fw.write(new char[]{'a', 'p', 'p', 'l', 'e'});
+
+        }catch (IOException e){
+            e.printStackTrace();
+            // catch 절 안에서는 예외로그를 출력하던, 다시 예외를 던지건 절대 비워둬서는 안된다.
+
+        }
+    }
+}
