55f3ca363086f87ead725cd578757197412d7a8c
Hongjimin <jmh907@naver.com>
2024-04-04 23:49:47 +0900

    Feat : 12_collections 복습

--- HongJimin/_12_collections_framework/src/main/java/com/sh/collections/_01/list/App1.java
+++ HongJimin/_12_collections_framework/src/main/java/com/sh/collections/_01/list/App1.java
@@ -33,26 +33,30 @@ public class App1 {
 
         //get(인덱스 : int)
         String elem0 = list.get(0); //리턴 타입 String 보장!
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
-        System.out.println(list.get(2));
-        System.out.println(list.get(3));
-        System.out.println(list.get(4));
+        System.out.println(list.get(0));//eeeee
+        System.out.println(list.get(1));//banana
+        System.out.println(list.get(2));//call
+        System.out.println(list.get(3));//abc
+        System.out.println(list.get(4));//data
 //        System.out.println(list.get(5)); //IndexOutOfBoundsException :Index 5 out of bounds for length 5
 
+
         //삽입 add(int, E)
-        list.add(2, "ㅋㅋㅋ");
+        list.add(2, "ㅋㅋㅋ"); //2번째 인덱스에, 새로운 값을 삽입, 나머지 값들은 인덱스 뒤로 밀림
+        System.out.println(list);
 
         //대체 set(int, E)
-        list.set(2, "ㅎㅎㅎ");
+        list.set(2, "ㅎㅎㅎ"); //2번째 인덱스 대신 값을 대체함
+        System.out.println(list);
 
         //제거 remove(int)
         list.remove(2);
+        System.out.println(list);
 
         //정렬 - 기본 오름차순 정렬
         Collections.sort(list); //원본 리스트를 직접 수정해줌
 
-        //내림차순 정렬
+        //내림차순 정렬 🆘 어려워융
         Comparator<String> comparator = Collections.reverseOrder(); //정렬 기준 객체
         Collections.sort(list, comparator);
 
@@ -60,7 +64,7 @@ public class App1 {
         System.out.println(list);
 
         //저장된 요소의 개수 = size() : int
-        System.out.println(list.size());
+        System.out.println(list.size()); //5
 
         //반복 처리
         for (int i = 0; i < list.size(); i++) {
@@ -75,10 +79,10 @@ public class App1 {
         // - hasNext() : 다음 요소 존재 여부 반환
         // - next() : 다음 요소 반환
         Iterator<String> iter = list.iterator();
-        while(iter.hasNext()){ //토큰과 사용이 비슷함!
-            String next = iter.next();
-            System.out.println(next);
-        }
+//        while(iter.hasNext()){ //토큰과 사용이 비슷함!
+//            String next = iter.next();
+//            System.out.println(next);
+//        }
 
         //iterator 객체는 요소를 소비한다. 모두 소비 후에는 hasNext에서는 항상 false를 반환
         while(iter.hasNext()){ //토큰과 사용이 비슷함!
