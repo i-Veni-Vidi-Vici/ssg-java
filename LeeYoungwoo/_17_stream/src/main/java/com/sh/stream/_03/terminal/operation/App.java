fb13e3ee9f73f6384e678148f01afd7d0a8ed48e
youngwoo2 <leeduddn98@gmail.com>
2024-04-05 16:18:26 +0900

    Feat : stream/thread

---
+++ LeeYoungwoo/_17_stream/src/main/java/com/sh/stream/_03/terminal/operation/App.java
@@ -0,0 +1,30 @@
+package com.sh.stream._03.terminal.operation;
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+/**
+ * <pre>
+ * 집계연산 지원
+ * - 합, 평균, 최대값, 최소값을 구할 수 있다.
+ * </pre>
+ */
+public class App {
+    public static void main(String[] args) {
+        int sum = IntStream.of(1,2,3,4,5,6,7,8,9,10).sum();
+        System.out.println(sum);
+
+        // Optional : null일 수 있는 값을 감싼 객체. null처리 지원 메소드 제공. 갯수가 1개인 Stream이다.
+        OptionalDouble average = IntStream.of(1,2,3,4,5,6,7,8,9,10).average();
+        System.out.println(average.getAsDouble());
+
+        OptionalInt max = IntStream.of(1,2,3,4,5,6,7,8,9,10).max();
+        System.out.println(max.getAsInt());
+
+        OptionalInt min = IntStream.of(1,2,3,4,5,6,7,8,9,10).min();
+        System.out.println(min.getAsInt());
+
+    }
+}
