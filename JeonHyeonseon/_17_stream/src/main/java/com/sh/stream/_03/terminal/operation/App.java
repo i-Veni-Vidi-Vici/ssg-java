e4195f499079cd684e25541c326603db4322eee8
jeonhyeonseon <good_pretty@naver.com>
2024-04-05 16:25:58 +0900

    Feat: 스트림중계-단말연산/쓰레드생성

---
+++ JeonHyeonseon/_17_stream/src/main/java/com/sh/stream/_03/terminal/operation/App.java
@@ -0,0 +1,29 @@
+package com.sh.stream._03.terminal.operation;
+
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+/**
+ * <pre>
+ * 집계연산 지원
+ *  - 합, 평균, 최대값, 최소값을 구할 수 있다.
+ * </pre>
+ */
+public class App {
+    public static void main(String[] args) {
+        int sum = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).sum(); // 단말연산자
+        System.out.println(sum);
+        // Optional : null일 수 있는 값을 감싼 객체. null처리 지원메소드 제공. 개수가 1개인 Stream처럼 생각하면 된다.
+        OptionalDouble average = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).average();
+        System.out.println(average.getAsDouble()); // 5.5
+
+        // 최대값
+        OptionalInt max = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).max();
+        System.out.println(max.getAsInt());
+
+        // 최소값
+        OptionalInt min = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).min();
+        System.out.println(min.getAsInt());
+    }
+}
