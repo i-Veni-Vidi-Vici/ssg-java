f32e1cc9e5fef6ba29d4cf55fbe1d4eb371e27eb
jyna120 <jyna120@naver.com>
2024-04-06 23:59:35 +0900

    Feat: 예외처리 실습문제

---
+++ NaJiyoung/_13_exception_handling/src/main/java/com/ssg/exception/number/NumberRangeException.java
@@ -0,0 +1,7 @@
+package com.ssg.exception.number;
+
+public class NumberRangeException extends RuntimeException {
+    public NumberRangeException(String message) {
+        super(message);
+    }
+}
