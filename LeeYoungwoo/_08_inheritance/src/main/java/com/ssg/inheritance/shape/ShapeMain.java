0511a63ae1c71f44270ffa99352aa3be2adbd815
BOOK-QFJTSFASTP\leedu <leeduddn98@gmail.com>
2024-04-08 18:49:09 +0900

    Feat: 상속 실습문제2 풀이

--- LeeYoungwoo/_08_inheritance/src/main/java/com/ssg/inheritance/shape/ShapeMain.java
+++ LeeYoungwoo/_08_inheritance/src/main/java/com/ssg/inheritance/shape/ShapeMain.java
@@ -7,6 +7,7 @@ public class ShapeMain {
         Circle circle = new Circle(new Point(100, 100), 10);
         circle.draw();
         System.out.println("원의 넓이는 " + circle.getArea() + "입니다.");
+        System.out.println();
 
         // rect1
 //        Point[] points = {
@@ -24,9 +25,9 @@ public class ShapeMain {
 //        Rectangle rect2 = new Rectangle(new Point(0,0), 200, 100);
 //        rect2.draw();
 //        System.out.println("사각형의 넓이는 " + rect2.getArea() + "입니다.");
-//
-//        Rectangle rect3 = new Rectangle(new Point(0,0), new Point(200,100));
-//        rect3.draw();
-//        System.out.println("사각형의 넓이는 " + rect3.getArea() + "입니다.");
+
+        Rectangle rect3 = new Rectangle(new Point(0,0), new Point(200,100));
+        rect3.draw();
+        System.out.println("사각형의 넓이는 " + rect3.getArea() + "입니다.");
     }
 }
