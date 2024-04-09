5ab81413d431654f97b7de5c26730c9e0c7e83ca
seongyeongit <wjd12118@nate.com>
2024-04-09 16:36:45 +0900

    Feat:쓰레드동기화(저수준)/네트워크/InetAddress/URL/Socket

--- ChungSeongYeon/_17_stream/src/main/java/com/sh/stream/_03/terminal/operation/App.java
+++ ChungSeongYeon/_17_stream/src/main/java/com/sh/stream/_03/terminal/operation/App.java
@@ -15,7 +15,10 @@ public class App {
     public static void main(String[] args) {
         int sum = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).sum();
         System.out.println(sum);
-        // Optional : null일 수 있는 값을 감싼 객체. null 처리 지원메소드 제공. 개수가 1개인 Stream이라고 생각하면 좋다.
+        // Optional
+        // - null일 수 있는 값을 감싼 객체.
+        // - null 처리 지원메소드 제공.
+        // - 개수가 1개인 Stream이다.
         OptionalDouble average = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).average(); // 평균값을 감싼 Optional 객체
         System.out.println(average.getAsDouble()); // 5.5
 
@@ -24,6 +27,9 @@ public class App {
 
         OptionalInt min = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).min();
         System.out.println(min.getAsInt());
+
+        long count = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).count();
+        System.out.println(count);
 
 
     }
