8576a400dbd6886a96c05834aeb0803dcce64f9f
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-04-09 16:37:16 +0900

    Feat: 18_thread 추가

--- JeongYejin/_18_thread/src/main/java/com/sh/thread/_01/basic/App3.java
+++ JeongYejin/_18_thread/src/main/java/com/sh/thread/_01/basic/App3.java
@@ -17,6 +17,8 @@ public class App3 {
         Runnable runnable2 = new MyRunnable('-');
         Thread th1 = new Thread(runnable1);
         Thread th2 = new Thread(runnable2);
+        th1.setName("일꾼1"); // name은 default: main, Thread-0, Thread-1
+        th2.setName("일꾼2");
         th1.start(); // 각 쓰레드별 새 콜스택을 할당 받아서 작업을 시작
         th2.start();
         System.out.print(Thread.currentThread().getName());
