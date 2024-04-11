ad2f63c94832cabd7c119a300feb6db838beac60
subkka <beeniaa@naver.com>
2024-04-11 12:36:55 +0900

    Feat: Concurrent API - Executors

---
+++ ParkSubeen/_18_thread/src/main/java/com/sh/thread/_03/concurrent/executors/App4.java
@@ -0,0 +1,36 @@
+package com.sh.thread._03.concurrent.executors;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <pre>
+ * Executors.newSingleThreadScheduledExecutor()
+ * - n초뒤 작업실행, m초간격을 작업 반복실행등을 수헹하는 Executor
+ * </pre>
+ */
+public class App4 {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        // n초뒤에 작업실행
+        scheduledExecutorService.schedule(() -> System.out.println("Helloworld"), 5, TimeUnit.SECONDS);
+
+        // m초간격으로 작업실행
+        scheduledExecutorService.scheduleAtFixedRate(() -> System.out.println(LocalDateTime.now()), 5, 1, TimeUnit.SECONDS);
+
+        System.out.println("12345");
+
+
+        try {
+            Thread.sleep(10 * 1000);
+            scheduledExecutorService.shutdown();
+            // 일정간격이후 executorService만 종료
+//            scheduledExecutorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
