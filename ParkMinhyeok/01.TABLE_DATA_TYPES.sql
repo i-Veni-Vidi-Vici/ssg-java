0b4370a4a7b74cab269e23c2896059dc3524a5a3
ParkMinhyeok <parkminem@gmail.com>
2024-04-12 10:48:03 +0900

    Feat: sh계정/menudb 생성 #2

--- ParkMinhyeok/01.TABLE_DATA_TYPES.sql
+++ ParkMinhyeok/01.TABLE_DATA_TYPES.sql
@@ -1,4 +1,9 @@
 show databases;
 use menudb; -- 특정 database 를 선택하고, 테이블 작업이 가능하다.
 
-
+select * from tbl_menu; -- 메튜
+select * from tbl_category; -- 카테고리
+select * from tbl_order; -- 주문내역
+select * from tbl_order_menu; -- 주문별 메뉴정보
+select * from tbl_payment; -- 결제
+select * from tbl_payment_order;
