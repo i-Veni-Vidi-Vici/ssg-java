d92f56b4852f486123a01ca41d8ef5b488736c46
DESKTOP-2JVLHQE\Park <beeniaa@naver.com>
2024-04-14 16:13:29 +0900

    Feat: 쓰레드 실습문제 제출

--- ParkSubeen/_18_thread/src/main/java/com/ssg/thread/sleep/SleepTest.java
+++ ParkSubeen/_18_thread/src/main/java/com/ssg/thread/sleep/SleepTest.java
@@ -1,9 +1,8 @@
 package com.ssg.thread.sleep;
 
+import java.util.HashSet;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Set;
 
 public class SleepTest {
     public static void main(String[] args) {
@@ -12,7 +11,7 @@ public class SleepTest {
     }
 
     public void sendAphorism() {
-        String[] str = {"계란으로 바위치기",
+        String[] wiseSaying = {"계란으로 바위치기",
                         "고기도 먹어본 사람이 많이 먹는다",
                         "고래 싸움에 새우 등 터진다",
                         "고양이한테 생선 맡겼다",
@@ -25,15 +24,60 @@ public class SleepTest {
                         };
 
         Random rnd = new Random();
+        Set<String> strSet = new HashSet<>();
+        while(strSet.size() < 10) {
+            int random = rnd.nextInt(10);
+            strSet.add(wiseSaying[random]);
+        }
+
 
         try {
-            int random = rnd.nextInt(10);
-            ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
-            ses.scheduleAtFixedRate(() -> System.out.println(str[random]), 10, 1, TimeUnit.SECONDS);
-            Thread.sleep(3000);
-            ses.shutdown();
+            for(String str : strSet) {
+                System.out.println(str);
+                Thread.sleep(3000);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
+//    public void sendAphorism() {
+//        String messages[] = {
+//                "Pride will have a fall.",
+//                "Power is dangerous unless you have humility.",
+//                "Office changes manners.",
+//                "Empty vessels make the most sound.",
+//                "Heaven helps those who help themseleves.",
+//                "Things change.",
+//                "Forgive yorself before you die. Then forgive others.",
+//                "Don't let go too soon, but don't hang on too soon.",
+//                "Accept what you are able to do and what you are not able to do.",
+//                "Accept what past as past, whthout denying or discarding it."
+//        };
+//
+//
+//        //0.String[] => List<String>
+//        List<String> temp = Arrays.asList(messages);
+//
+//        //1.순서섞기
+//        Collections.shuffle(temp);
+////		System.out.println(temp);
+//
+//
+//        //2.Queue생성
+//        Queue<String> queue = new LinkedList<>(temp);
+//
+//        // 추가 offer(E), 제거 poll():E
+//        while(queue.size() > 0) {
+//            System.out.println(queue.poll());
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
+
+
+
