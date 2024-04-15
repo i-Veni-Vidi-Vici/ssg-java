11573d956522d90c265c987375e4e22a5d96a5bd
jeongeun1 <wjddms6188@gmail.com>
2024-04-15 17:34:36 +0900

    Update: 단일행함수 실습문제 수정 #91

--- ParkJeongeun/emp/02_단일행함수 실습문제.sql
+++ ParkJeongeun/emp/02_단일행함수 실습문제.sql
@@ -4,7 +4,7 @@ select
     insert(EMP_NO, 8, 7,'*******'),
     SALARY
 from employee
-where substring(EMP_NO, 8, 1) = 1 ;
+where substring(EMP_NO, 8, 1) = 1 || substring(EMP_NO, 8, 1) = 3;
 
 select
     EMP_NAME,
