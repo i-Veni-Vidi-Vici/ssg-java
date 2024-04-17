c660419cc57063c342a70c009ef02f84463ef5ba
Hong-ji-min <jmh907@naver.com>
2024-04-17 15:08:37 +0900

    Feat : 04_DQL실습문제2 - group by,having 1, 3, 4 문제풀이

--- HongJimin/Homework/01_DQL_practice1/emp/04_DQL실습문제2 - group by,having.sql
+++ HongJimin/Homework/01_DQL_practice1/emp/04_DQL실습문제2 - group by,having.sql
@@ -12,34 +12,36 @@ group by
 
 -- 2. EMPLOYEE테이블에서 직급이 J1을 제외하고, 입사년도별 인원수를 조회해서, 입사년 기준으로 오름차순 정렬하세요.
 select
-    extract(year from HIRE_DATE) ,
+    extract(year from HIRE_DATE),
     count(*)
 from
     employee
 where
     JOB_CODE != 'J1'
 group by
-    extract(year from HIRE_DATE);
+    extract(year from HIRE_DATE) ASC;
 -- ⛳⛳⛳오름차순 정렬 기능 해야함!!
 
 -- 3. 성별 급여의 평균(정수처리), 급여의 합계, 인원수를 조회한 뒤 인원수로 내림차순을 정렬 하시오.
 select
-    case
-        when
-            substring(EMP_NO, 8, 1) = '1' or substring(EMP_NO, 8, 1) = '3'
-            then avg(SALARY)
-        when
-            substring(EMP_NO, 8, 1) = '2' or substring(EMP_NO, 8, 1) = '4'
-            then avg(SALARY)
-    end as '얼'
-from
-    employee;
-
-
--- 4. 직급별 인원수가 3명이상이 직급과 총원을 조회
-
-select
-    *
+    floor(avg(SALARY)) 급여평균,
+    sum(SALARY) 급여합계,
+    concat(count(*), '명') 인원수
 from
     employee
+group by
+    substring(EMP_NO, 8, 1) = '1' || substring(EMP_NO, 8, 1) = '3',
+    substring(EMP_NO, 8, 1) = '2' || substring(EMP_NO, 8, 1) = '4';
+
+-- 4. 직급별 인원수가 3명이상인 직급과 총원을 조회
+select
+    JOB_CODE 직급,
+    concat(count(*), '명') 총원
+from
+    employee
+group by
+    JOB_CODE
+having
+    count(*) >= 3;
+
 
