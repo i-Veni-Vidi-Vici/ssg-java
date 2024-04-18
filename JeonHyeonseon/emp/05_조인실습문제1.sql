046919fde614d4798ae2f7400fb09526e1697c46
jeonhyeonseon <good_pretty@naver.com>
2024-04-18 10:00:37 +0900

    Update: 조인실습문제 문제 추가 & 리뷰

--- JeonHyeonseon/emp/05_조인실습문제1.sql
+++ JeonHyeonseon/emp/05_조인실습문제1.sql
@@ -40,6 +40,9 @@ from
     employee e join department d
 
 -- 4. 이름에 '형'자가 들어가는 직원들의 사번, 사원명, 부서명을 조회하시오.
+-- inner join 부서코드 미지정된 사원은 제외됨 -> 이름에 '형'자가 들어간 사람이 부서코드가 미지정일 수도 있다.
+-- left outer join : 부서코드가 미지정인 사원도 포함됨
+-- 따라서 left outer join을 해야한다.
 select * from employee;
 select * from department;
 
@@ -48,20 +51,70 @@ select
     e.EMP_NAME 사원명,
     d.DEPT_TITLE 부서명
 from
-    employee e join department d
+    employee e left join department d
         on e.DEPT_CODE = d.DEPT_ID
 where
     EMP_NAME like '%형%';
 
 -- 5. 해외영업부에 근무하는 사원명, 직급명, 부서코드, 부서명을 조회하시오.
-select * from nation; -- national_code, national_name
 select * from employee;
 select * from job; -- job_code, job_name
 select * from department; -- dept_id, dept_title, location_id
 
 select
+    e.EMP_NAME 사원명,
+    j.JOB_NAME 직급명,
+    e.DEPT_CODE 부서코드,
+    d.DEPT_TITLE 부서명
+from employee e
+    join department d
+        on e.DEPT_CODE = d.DEPT_ID
+    join job j
+        on e.JOB_CODE = j.JOB_CODE
+where
+    DEPT_TITLE like '해외%';
 
-from
-    employee
+-- 6. 보너스포인트를 받는 직원들의 사원명, 보너스포인트, 부서명, 근무지역명을 조회하시오.
+select * from employee;
+select * from department;
+select * from location;
 
+select
+    e.EMP_NAME 사원명,
+    e.BONUS 보너스포인트,
+    d.DEPT_TITLE 부서명,
+    l.LOCAL_NAME 근무지역명
+from employee e
+    join department d
+        on e.DEPT_CODE = d.DEPT_ID
+    join location l
+        on d.LOCATION_ID = l.LOCAL_CODE
+where
+    ifnull(BONUS, 0);
 
+-- 7. 부서코드가 D2인 직원들의 사원명, 직급명, 부서명, 근무지역명을 조회하시오.
+select * from employee;
+select * from job;
+select * from department;
+select * from location;
+
+select
+    EMP_NAME 사원명,
+    JOB_NAME 직급명,
+    DEPT_TITLE 부서명,
+    LOCAL_NAME 근무지역명
+from employee e
+    join job j
+        on e.JOB_CODE = j.JOB_CODE
+    join department d
+        on e.DEPT_CODE = d.DEPT_ID
+    join location l
+        on d.LOCATION_ID = l.LOCAL_CODE
+where
+    DEPT_CODE = 'D2';
+
+-- 8. 급여등급테이블 sal_grade의 등급별 최대급여(MAX_SAL)보다 많이 받는 직원들의 사원명, 직급명, 급여, 연봉을 조회하시오.
+--    (사원테이블과 급여등급테이블을 SAL_LEVEL컬럼기준으로 동등(등가) 조인할 것)
+select * from employee;
+select * from job;
+select * from sal_grade;
