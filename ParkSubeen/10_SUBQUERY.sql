0b688af2f1e198dc961a4f4fe33afd025e8b43b6
subkka <beeniaa@naver.com>
2024-04-19 17:24:37 +0900

    Feat: 상관서브쿼리/인라인뷰 #73

--- ParkSubeen/10_SUBQUERY.sql
+++ ParkSubeen/10_SUBQUERY.sql
@@ -96,3 +96,278 @@ where
         where
             ref_category_code = 1
         );
+
+-- ----------------------------------------------
+-- 상관 서브쿼리
+-- ----------------------------------------------
+-- 상호연관 서브쿼리.
+-- 메인쿼리의 값을 서브쿼리에 전달해서 수행 후에 그 결과를 다시 메인쿼리로 반환하는 방식
+-- 메인쿼리의 레코드에 따라 수행할 서브쿼리가 달라진다.
+-- 상관서브쿼리만 단독으로 블럭잡아 실행할 수 없다.
+
+# 구분
+-- 일반서브쿼리 : 단독으로 블럭잡아 실행 가능
+-- 상관서브쿼리 : 단독으로 블럭잡아 실행 불가.
+
+-- 카테고리별로 가격이 가장 비싼 메뉴 조회
+-- 1. 조인으로 해결하기
+
+select
+    category_code,
+    max(menu_price)
+from
+    tbl_menu
+group by
+    category_code;
+
+-- inner join (catefgory_code가 null인 그룹 제외)
+select
+    *
+from
+    tbl_menu m join (
+        select
+            category_code,
+            max(menu_price) max_menu_price
+        from
+            tbl_menu
+        group by
+            category_code
+    ) c
+        on m.category_code = c.category_code
+where
+    m.menu_price = c.max_menu_price
+order by
+    m.category_code;
+
+-- outer join (category_code가 null인 그룹도 포함)
+select
+    *
+from
+    tbl_menu m left join (
+        select
+            category_code,
+            max(menu_price) max_menu_price
+        from
+            tbl_menu
+        group by
+            category_code
+    ) c
+        -- null끼리 동일비교 할 수 없으므로, ifnull함수를 이용해서 특정 값으로 변환 후 비교
+#         on ifnull(m.category_code, 0) = ifnull(c.category_code, 0)
+        on ifnull(m.category_code, 1000) = ifnull(c.category_code, 1000) -- 특정 값으로 변환해서 비교하기 때문에 0이 아닌 값도 가능
+where
+    m.menu_price = c.max_menu_price
+order by
+    m.category_code;
+
+-- 2. 상관서브쿼리로 해결하기
+select
+    *
+from
+    tbl_menu m
+where
+#     menu_price = (카테고리별 최고 비싼 금액);
+    menu_price = (
+    select
+        max(menu_price)
+    from
+        tbl_menu
+    where
+        category_code = m.category_code
+    );
+-- 메인 쿼리 where절에서 매행마다 다음 쿼리를 수행한다. 이 행의 category가 전달된다.
+select
+    *
+from
+    tbl_menu
+where
+    category_code = 11; -- 우항에 메인쿼리 레코드의 category_code가 대입
+
+-- 카테고리별 평균가격보다 높은 가격의 메뉴 조회
+-- 메인쿼리 테이블 별칭이 반드시 필요하다
+select
+    *
+from
+    tbl_menu m
+where
+#     menu_price > (같은 카테고리의 평균가격);
+menu_price >= (
+    select
+        avg(menu_price)
+    from
+        tbl_menu
+    where
+        category_code = m.category_code
+);
+
+
+# exists 연산자
+-- exists (서브쿼리) 서브쿼리의 조회된 행이 존재하면 true, 존재하지 않으면 false
+
+-- 메뉴가 있는 카테고리 조회
+select
+    *
+from
+    tbl_category c
+where
+    exists(
+        select
+            1 -- 내용은 중요하지 않다, 행이 존재하는가의 여부가 중요
+        from
+            tbl_menu
+        where
+            category_code = c.category_code
+    );
+
+use empdb;
+-- department테이블에서 실제 부서원이 존재하는 부서만 조회
+-- 메인쿼리의 테이블은 조회하는 테이블이 온다.
+select
+    *
+from
+    department d
+where
+    exists(
+        select
+            *
+        from
+            employee
+        where
+            DEPT_CODE = d.DEPT_ID
+    );
+
+-- department테이블에서 유령부서 조회
+-- not exists(서브쿼리) 서브쿼리 행이 존재하면 참!
+select
+    *
+from
+    department d
+where
+#     !exists(
+    not exists(
+        select
+            *
+        from
+            employee
+        where
+            DEPT_CODE = d.DEPT_ID
+    );
+
+-- not exists를 이용한 최대/최소 행 구하기
+-- 가격이 가장 비싼 메뉴 조회
+use menudb;
+
+select
+    *
+from
+    tbl_menu m
+where
+    not exists(
+        select
+            *
+        from
+            tbl_menu
+        where
+            menu_price > m.menu_price
+    );
+
+-- 가격이 가장 싼 메뉴 조회
+select
+    *
+from
+    tbl_menu m
+where
+    not exists(
+        select
+            *
+        from
+            tbl_menu
+        where
+            menu_price < m.menu_price
+    );
+
+# 스칼라 서브쿼리
+-- 실행결과가 하나(스칼라)인 상관서브쿼리. 주로 select절에서 사용한다.
+-- 다른 테이블의 여러개의 값을 조회하는게 아니면 join 대신 사용하는 것이 좋을 것 같다.
+
+-- 메뉴명, 카테고리명 조회
+select
+    menu_name,
+    (select category_name from tbl_category where category_code = m.category_code) category_name
+from
+    tbl_menu m;
+
+-- 카테고리테이블에서 카테고리명, 상위카테고리명 조회
+select
+    category_name,
+    (select category_name from tbl_category where category_code = c.ref_category_code) ref_category_name
+from
+    tbl_category c;
+
+-- ---------------------------------
+-- 인라인 뷰
+-- ---------------------------------
+-- from절에 사용한 서브쿼리를 가리킨다.
+-- 마치 하나의 테이블인 것처럼 사용할 수 있다.
+
+# 뷰 구분
+-- 1. 인라인 뷰 : 1회용 서브쿼리
+-- 2. 저장 뷰 : db객체 중의 하나로, 뷰를 테이블처럼 재사용 가능
+select
+#     category_code, -- [42S22][1054] Unknown column 'category_code' in 'field list'
+#     category_name, -- 인라인뷰에서 별칭을 사용한 경우 별칭으로 참조해야 한다.
+    카테고리명,
+    ref_category_name
+from (
+         select
+             category_name 카테고리명,
+             (select category_name from tbl_category where category_code = c.ref_category_code) ref_category_name
+         from
+             tbl_category c
+) t;
+
+-- 메뉴테이블에서 카테고리별 메뉴개수를 집계했을때 최대/최소값 조회
+-- 인라인뷰?
+select
+    max(메뉴개수),
+    min(메뉴개수)
+from(
+    select
+        category_code,
+        count(*) 메뉴개수
+    from
+        tbl_menu
+    group by
+        category_code
+    ) t;
+
+# common table expression
+-- 인라인뷰에 이름을 지정하고, 이를 참조하는 방식
+
+with menu
+as (
+    select
+        *
+    from
+        tbl_menu
+    )
+select
+    *
+from
+    menu;
+
+with cnt_by_category
+as (
+        select
+            category_code,
+            count(*) 메뉴개수
+        from
+            tbl_menu
+        group by
+            category_code
+    )
+select
+    max(메뉴개수),
+    min(메뉴개수)
+from
+    cnt_by_category;
