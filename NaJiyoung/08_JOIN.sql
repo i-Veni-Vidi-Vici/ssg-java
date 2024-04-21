bb55f51d8da9cd0121a4657af134076292a4b30b
jyna120 <jyna120@naver.com>
2024-04-21 23:43:24 +0900

    Feat: 집합연산자, 조인추가 #72

--- NaJiyoung/08_JOIN.sql
+++ NaJiyoung/08_JOIN.sql
@@ -201,6 +201,11 @@ select
 from
     tbl_menu m /* cross */ join tbl_category c;
 
+select
+    *
+from
+    tbl_menu m, tbl_category c;
+
 -- ----------------------------------
 -- SELF JOIN
 -- ----------------------------------
