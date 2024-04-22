5807e2cc799bb8b2762c49b8fe710d2210e47767
vlshzl35 <vlshzl35@naver.com>
2024-04-22 14:59:57 +0900

    춘대학 Additional SELECT 함수 실습문제2 #165

--- YangHeeyun/chun/02_춘대학_AdditionalSELECT함수_실습문제2.sql
+++ YangHeeyun/chun/02_춘대학_AdditionalSELECT함수_실습문제2.sql
@@ -51,64 +51,87 @@ where
 
 -- 5.춘 기술대학교의 재수생 입학자를 구하려고 한다. 어떻게 찾아낼 것인가?
 -- 이때, 만 19살이 되는 해에 입학하면 재수를 하지 않은 것으로 간주한다.
+-- 입학년도-출생년도
 select
-    STUDENT_NAME,
-    year(ENTRANCE_DATE) - (if(mid(STUDENT_SSN,8,1) = '1' or mid(STUDENT_SSN,8,1) = '2',1900,2000) + left(STUDENT_SSN,2)) 입학나이
+    STUDENT_NO, STUDENT_NAME,
+    year(ENTRANCE_DATE) - (if(mid(STUDENT_SSN,8,1) IN (1,2),1900,2000) + left(STUDENT_SSN,2)) "입학당시 만나이"
 from
     tb_student
 where
-    year(ENTRANCE_DATE) - (if(mid(STUDENT_SSN,8,1) = '1' or mid(STUDENT_SSN,8,1) = '2',1900,2000) + left(STUDENT_SSN,2)) > 19;
+    year(ENTRANCE_DATE) - (if(mid(STUDENT_SSN,8,1) IN (1,2),1900,2000) + left(STUDENT_SSN,2)) > 19;
 
 
 -- 6.2020년 크리스마스는 무슨 요일이었는가?
 select
     date_format('2020-12-25','%W') 크리스마스;
 
--- 7.4. *`STR_TO_DATE*('99/10/11', '%y/%m/%d')` *`STR_TO_DATE*('49/10/11', '%y/%m/%d')`은 각각 몇 년 몇 월 몇 일을 의미할까?
--- 또 *`STR_TO_DATE*('70/10/11', '%y/%m/%d')` *`STR_TO_DATE*('69/10/11', '%y/%m/%d')` 은 각각 몇 년 몇 월 몇 일을 의미할까?
+-- 7.4.STR_TO_DATE('99/10/11', '%y/%m/%d'),STR_TO_DATE('49/10/11', '%y/%m/%d')은 각각 몇 년 몇 월 몇 일을 의미할까?
+-- 또 STR_TO_DATE('70/10/11', '%y/%m/%d'), STR_TO_DATE('69/10/11', '%y/%m/%d') 은 각각 몇 년 몇 월 몇 일을 의미할까?
+-- 70 ~ 99년은 1900년대로 인식. 69년이하는 2000년대로 인식함
+select STR_TO_DATE('99/10/11', '%y/%m/%d'); -- 1999/10/11
+select STR_TO_DATE('49/10/11', '%y/%m/%d'); -- 2049/10/11
+select STR_TO_DATE('70/10/11', '%y/%m/%d'); -- 1970/10/11
+select STR_TO_DATE('69/10/11', '%y/%m/%d'); -- 2069/10/11
 
 -- 8. 학번이 A517178 인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오.
 -- 단, 이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
+select
+    student_name,
+    format((select avg(point) from tb_grade where student_no = 'A517178'),1) 평점
+from
+    tb_student
+where
+    student_no = 'A517178';
+
 
 -- 9. 학과별 학생수를 구하여 "학과번호", "학생수(명)" 의 형태로 헤더를 만들어 결과값이 출력되도록 하시오.
-
--- 10. 지도 교수를 배정받지 못한 학생의 수는 몇 명 정도 되는 알아내는 SQL 문을 작성하시오.
-
--- 11. 학번이 A112113 인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오.
--- 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
-
-
--- 12. 학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을 작성하시오.
 select
-    DEPARTMENT_NO,
-    sum(if(ABSENCE_YN = 'Y',1,0))
+    DEPARTMENT_NO 학과번호,count(*) '학생수(명)'
 from
     tb_student
 group by
     DEPARTMENT_NO;
 
-SELECT
-    tb_student.DEPARTMENT_NO,
-    tb_department.DEPARTMENT_NAME,
-    COALESCE(SUM(CASE WHEN tb_student.ABSENCE_YN = 'Y' THEN 1 ELSE 0 END), 0) AS present_count
-FROM
+-- 10. 지도 교수를 배정받지 못한 학생의 수는 몇 명 정도 되는 알아내는 SQL 문을 작성하시오.
+select
+    count(*)
+from
     tb_student
-        JOIN
-    tb_department ON tb_student.DEPARTMENT_NO = tb_department.DEPARTMENT_NO
-GROUP BY
-    tb_student.DEPARTMENT_NO, tb_department.DEPARTMENT_NAME;
+where
+    COACH_PROFESSOR_NO is null;
+
+-- 11. 학번이 A112113 인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오.
+-- 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
+select
+    substr(term_no,1,4) 년도,
+    format(avg(point),1) 년도별평점
+from
+    tb_grade
+where
+    STUDENT_NO = 'A112113'
+group by
+    substr(term_no,1,4) -- 년도
+
+-- 12. 학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을 작성하시오.
+select
+    DEPARTMENT_NO 학과번호,
+    (select DEPARTMENT_NAME from tb_department where DEPARTMENT_NO = s.DEPARTMENT_NO) 학과명,
+    sum(if(ABSENCE_YN = 'Y',1,0)) 휴학생수
+from
+    tb_student s
+group by
+    DEPARTMENT_NO;
 
 -- 13. 춘 대학교에 다니는 동명이인(同名異人) 학생들의 이름을 찾고자 한다. 어떤 SQL 문장을 사용하면 가능하겠는가?
 select
-    STUDENT_NAME
+    STUDENT_NAME,
+    count(*) '동명인 수'
 from
     tb_student
 group by
     STUDENT_NAME
 having
     count(*) > 1;
-
--- join 이름은 같은데 학번이 다른 경우
 
 -- 14.학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점 , 총평점을 구하는 SQL 문을 작성하시오.
 -- (단, 평점은 소수점 1 자리까지만 반올림하여 표시한다.
@@ -125,13 +148,15 @@ group by
     substr(TERM_NO,5,2)
     with rollup;
 
-SELECT
-    LEFT(TERM_NO, 4) AS 년도,
-    CASE WHEN GROUPING(SUBSTRING(TERM_NO, 5, 2)) = 0 THEN IFNULL(SUBSTRING(TERM_NO, 5, 2), 'N/A') ELSE '총 평점' END AS 학기,
-    ROUND(AVG(POINT), 1) AS 평점
-FROM tb_grade
-WHERE STUDENT_NO = 'A112113'
-GROUP BY LEFT(TERM_NO, 4), SUBSTRING(TERM_NO, 5, 2) WITH ROLLUP;
+-- grouping 사용해서 null 처리하는 버전
+-- 처리순서 group by + with roll up -> select grouping
+select distinct
+    if(grouping(left(TERM_NO, 4)) = 0, left(TERM_NO, 4), '총 평점') 년도,
+    if(grouping(substring(TERM_NO, 5, 2)) = 0, substring(TERM_NO, 5, 2), '학기 별 평점') 학기,
+    round(avg(POINT), 1) 평점
+from tb_grade
+where STUDENT_NO = 'A112113'
+group by left(TERM_NO, 4), substring(TERM_NO, 5, 2), TERM_NO with rollup;
 
 -- join 사용해서
 SELECT gr.year, gr.month_case, AVG(gr.point)
