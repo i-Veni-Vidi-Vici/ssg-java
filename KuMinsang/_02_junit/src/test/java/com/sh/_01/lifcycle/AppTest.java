9b5c4023395d54134e95ebfbd0f37bc88d09ff79
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-05-08 16:36:29 +0900

    Feat: JUnit 테스트 환경설정/단정문 #2

--- KuMinsang/_02_junit/src/test/java/com/sh/_01/lifcycle/AppTest.java
+++ KuMinsang/_02_junit/src/test/java/com/sh/_01/lifcycle/AppTest.java
@@ -6,13 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * 테스팅 기본 작성법
- * 테스트 클래스도 하나의 클래스이다
- * 테스트용 메소드에 @Test 어노테이션을 사용
- * @Test 메소드의 접근제한자는 private일수 없다.
- * 여러개의 @Test메소드는 실행순서를 보장하지 않는다. (각 테스트 메소드는 독립적이야 한다.)
- * @Test 메소드 실행할때마다 새로운 테스트객체가 생성된다.
+ * <pre>
+ * JUnit5 구성요소
+ * - JUnit Platform 테스팅을 위한 기본프레임워크
+ * - JUnit Jupiter 5버젼용
+ * - JUnit Vintage 3,4버젼 하위호환용
  *
+ * 테스팅 기본작성법
+ * - 테스트클래스도 하나의 클래스이다.
+ * - 테스트용메소드에 @Test 어노테이션을 사용
+ * - @Test 메소드의 접근제한자는 private일 수 없다.
+ * - 여러개의 @Test메소드는 실행순서를 보장하지 않는다. (각 테스트 메소드는 독립적이어야 한다.)
+ * - @Test메소드 실행할때 마다 새로운 테스트객체가 생성된다.
+ *
+ * </pre>
  */
 class AppTest {
     //Fixture(테스트할 대상 또는 테스트에 필요한 객체) 준비
