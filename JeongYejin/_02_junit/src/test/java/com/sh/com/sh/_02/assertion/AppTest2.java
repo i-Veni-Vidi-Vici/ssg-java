098f8c219b1bf63838cf260c71092bf380788620
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-05-09 09:48:59 +0900

    Update: collection/예외 단정문 추가 #2

--- JeongYejin/_02_junit/src/test/java/com/sh/com/sh/_02/assertion/AppTest2.java
+++ JeongYejin/_02_junit/src/test/java/com/sh/com/sh/_02/assertion/AppTest2.java
@@ -1,13 +1,16 @@
 package com.sh.com.sh._02.assertion;
 
+import org.assertj.core.data.MapEntry;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AppTest2 {
     @DisplayName("문자열 단정문")
@@ -41,6 +44,20 @@ public class AppTest2 {
                         LocalDateTime.of(2024,5,31,23,59,59));
 
     }
+
+    /**
+     * <pre>
+     * satisfy 계열 : 요소하나씩 추가적으로 단정문이 필요한 경우
+     * - allSatisfy(Consumer) : 하위 단정문을 모두 만족하는지
+     * - anySatisfy(Consumer) : 하위 단정문 중 하나라도 만족하는지
+     * - noneSatisfy(Consumer) : 하위 단정문 모두 만족하지 않는지
+     *
+     * match 계열 : 요소별로 Predicate검사가 필요한 경우
+     * - allMatch(Predicate) : 모든 요소가 Predicate의 true를 반환하는지
+     * - anyMatch(Predicate) : 하나의 요소라도 Predicate의 true를 반환하는지
+     * - noneMatch(Predicate) : 모든 요소가 Predicate의 false를 반환하는지
+     * </pre>
+     */
     @DisplayName("List 단정문")
     @Test
     void test3() {
@@ -50,6 +67,31 @@ public class AppTest2 {
                 .allSatisfy((name)-> {
                     assertThat(name.length()).isGreaterThan(0);
                 })
+                .allMatch((name) -> !name.isEmpty())
                 .hasSize(3);
+    }
+
+    @DisplayName("Map 단정문")
+    @Test
+    void test4() {
+        Map<String, Integer> map = Map.of("홍길동", 33, "신사임당", 28, "이순신", 44);
+
+        assertThat(map)
+                .containsKey("홍길동")
+                .containsKeys("홍길동", "신사임당")
+                .doesNotContainKey("강감찬")
+                .containsValue(33)
+                .containsEntry("신사임당", 28)
+                .contains(MapEntry.entry("이순신", 44));
+    }
+
+    @DisplayName("예외 단정문")
+    @Test
+    void test5() {
+        assertThatThrownBy(()->{
+            throw new RuntimeException("으악!");
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("!");
+        // 이거 에러뜨게 어떻게 함?
     }
 }
