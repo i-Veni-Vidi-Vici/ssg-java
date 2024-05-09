90df0b5781bef0fb541c2d4c06d019f2032ebc7a
Yoonjeong-Shin <yjshin124@gmail.com>
2024-05-09 15:36:18 +0900

    Feat : mybatis_crud #4

---
+++ ShinYoonjeong/_03_mybatis_crud/src/main/java/com/sh/Main.java
@@ -0,0 +1,11 @@
+package com.sh;
+
+import com.sh.menu.view.MenuView;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new MenuView().mainMenu();
+    }
+}
