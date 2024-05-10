4d02889a949ded31e3e45e8a3e60bce8e1ca67d3
seongyeongit <wjd12118@nate.com>
2024-05-10 12:38:20 +0900

    Update: 주문트랜잭션/예외처리 #4

---
+++ ChungSeongyeon/_03_mybatis_crud/src/main/java/com/sh/order/model/dto/OrderMenuDto.java
@@ -0,0 +1,18 @@
+package com.sh.order.model.dto;
+
+import lombok.*;
+
+//@NoArgsConstructor // 기본생성자
+//@AllArgsConstructor // 모든 매개인자 생성자
+//@Getter
+//@Setter
+//@ToString
+@Data // @ToString, @EqualsAndHashCode, @Getter, @Setter, @RequiredArgsConstructor!
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderMenuDto {
+    private int orderCode;
+    private int menuCode;
+    private int orderAmount;
+
+}
