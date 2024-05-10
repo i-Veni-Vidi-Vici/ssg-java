2687c3ac1dc5f821c37ba0b6ebedcf7e5cedf91a
jeonhyeonseon <good_pretty@naver.com>
2024-05-10 15:21:19 +0900

    Feat: 조인쿼리처리 (collection/association) #7

--- JeonHyeonseon/_03_mybatis_crud/src/main/java/com/sh/order/model/dao/OrderMapper.java
+++ JeonHyeonseon/_03_mybatis_crud/src/main/java/com/sh/order/model/dao/OrderMapper.java
@@ -6,4 +6,8 @@ import com.sh.order.model.dto.OrderMenuDto;
 public interface OrderMapper {
     int insertOrder(OrderDto orderDto);
     int insertOrderMenu(OrderMenuDto orderMenuDto);
+
+    int countAll(); // 추상메소드
+
+    OrderDto findByOrderCode(int orderCode);
 }
