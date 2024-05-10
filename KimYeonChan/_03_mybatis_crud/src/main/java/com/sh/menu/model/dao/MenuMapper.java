0c559bc2072608a23da2b40bad918bf9afcde38d
hurryduck <tigeryck0812@gmail.com>
2024-05-10 12:38:20 +0900

    Update: 주문 트랜잭션 / 예외 처리 #4

--- KimYeonChan/_03_mybatis_crud/src/main/java/com/sh/menu/model/dao/MenuMapper.java
+++ KimYeonChan/_03_mybatis_crud/src/main/java/com/sh/menu/model/dao/MenuMapper.java
@@ -1,5 +1,6 @@
 package com.sh.menu.model.dao;
 
+import com.sh.menu.model.dto.CategoryDto;
 import com.sh.menu.model.dto.MenuDto;
 
 import java.util.List;
@@ -25,4 +26,10 @@ public interface MenuMapper {
     int deleteMenu(int menuCode);
 
     List<MenuDto> findByCategoryCode(int categoryCode);
+
+    List<CategoryDto> findAllCategory();
+
+    List<MenuDto> findMenuOrderable();
+
+    List<MenuDto> findMenuOrderableByCategoryCode(int categoryCode);
 }
---
+++ KimYeonChan/_03_mybatis_crud/src/main/java/com/sh/order/model/dto/OrderMenuDto.java
@@ -0,0 +1,19 @@
+package com.sh.order.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+// @RequiredArgsConstructor: final 생성자
+// @NoArgsConstructor // 기본생성자
+// @AllArgsConstructor // 모든 매개인자 생성자
+// @Getter
+// @Setter
+// @ToString
+// @Data
+@Data
+@AllArgsConstructor
+public class OrderMenuDto {
+    private int orderCode;
+    private int menuCode;
+    private int orderAmount;
+}
