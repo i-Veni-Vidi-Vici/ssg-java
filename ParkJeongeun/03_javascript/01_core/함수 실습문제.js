429b9c0825ca17794ec27e69861008790595df12
jeongeun1 <wjddms6188@gmail.com>
2024-05-29 18:03:37 +0900

    Feat: 함수 실습문제 ~ing #96

--- ParkJeongeun/03_javascript/01_core/함수 실습문제.js
+++ ParkJeongeun/03_javascript/01_core/함수 실습문제.js
@@ -1,40 +1,27 @@
-//1
-let z = 50;
-(function(){
-    let z = 55;
-    z += 45;
-    console.log(`z = ${z}`); // 100
-})();              
-console.log(`z = ${z}`); // 50
+// 2-1.
+const num1 = 10;
+const num2 = 20;
 
-//2
-let y = 99;
-(function() {
-    let x = 100;
-    let y = 9;
-    z = 1;
-    console.log(`x + y + z = ${x + y + z}`); // 110
-})();
-console.log(`y + z = ${y + z}`); // 100
-// console.log(`x + y + z = ${x + y + z}`); // 200 ★x is not defined
-
-//3
-let i; // 전역 i
-for (i = 1; i <= 10; i++) {
-	console.log(i); // 1~10까지 출력
+function min(a, b){
+    a > b ? console.log('최소값 : ' + b) : console.log('최소값 : ' + a)
 }
+min(num1, num2);
 
-(function(){
-	console.log(`i = ${i}`); // 11
-})();
-    
-//4 
-function foo(){
-    const num = 100;
-    return () => {
-        console.log(`num = ${num}`); // 100 ★여기서 출력X(밑에 too()에서 출력)
-    }
+function max(a, b){
+    a > b ? console.log('최대값 : ' + a) : console.log('최대값 : ' + b)
 }
-const num = 200;
-const too = foo();
-too(); // 100
+max(num1, num2);
+
+// 2-2.
+const x = 2;
+const n = 10;
+function pow(x, n){
+    if(typeof n == BigInt && n > 0){
+        for(let i = 1; i < n; i++){
+            x * x;
+        }
+    } else {
+        console.log(n + '은 양의 정수여야 합니다. 다시 입력해주세요.');
+    }    
+}
+pow(x, n);
