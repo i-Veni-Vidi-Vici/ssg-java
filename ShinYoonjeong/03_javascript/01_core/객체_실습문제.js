b3fab0374a94b05dffd233587f46798b41d87661
Yoonjeong-Shin <yjshin124@gmail.com>
2024-05-30 09:28:06 +0900

    Feat:e

---
+++ ShinYoonjeong/03_javascript/01_core/객체_실습문제.js
@@ -0,0 +1,12 @@
+const calculator = {
+    a : 10,
+    b : 20,
+    sum(a,b){
+        return a+b;
+        
+    },
+    multi(a,b){
+        return a*b;
+        }
+}
+console.log(calculator.sum);
