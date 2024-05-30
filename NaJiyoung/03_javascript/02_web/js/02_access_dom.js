72919dac6d559db14f1de004b16f359af394464e
jyna120 <jyna120@naver.com>
2024-05-30 17:12:11 +0900

    Feat: Javascript Web, DOM #109 #110

---
+++ NaJiyoung/03_javascript/02_web/js/02_access_dom.js
@@ -0,0 +1,44 @@
+console.log('🍖');
+
+/**
+ * document.getElementById(id)
+ */
+const test1 = () => {
+    const li1 = document.getElementById("li1");
+    console.log(li1, typeof li1);
+    console.dir(li1);
+    
+    li1.innerHTML = 'Hello1 JS';
+
+    const notExist = document.getElementById("zzzzz");
+    console.log(notExist);
+};
+
+
+/**
+ * document.getElementsByTagName(tagName)
+ * - 조회된 태그객체를 유사배열로 반환
+ */
+const test2 = () => {
+    const list = document.getElementsByTagName("li");
+    console.log(list);
+
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i]);
+        list[i].style.backgroundColor = 'springgreen';
+    }
+};
+
+/**
+ * document.getElementsByClassName(className)
+ * - 조회된 태그객체를 유사배열로 반환
+ */
+const test3 = () => {
+    const group1 = document.getElementsByClassName("group1");
+    console.log(group1);
+
+    for(let i = 0; i < group1.length; i++) {
+        const element = group1[i];
+        element.style.textDecoration = "underline";
+    }
+}
