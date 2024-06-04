92e8a62fad0e5a94ce2b804a12b51ddcbb5c0e08
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-06-04 16:40:20 +0900

    Feat: servlet 사용자 입력값 처리 #4

---
+++ KuMinsang/_03_request_parameter/src/main/java/com/sh/_03_request_parameter/RequestParameterServlet.java
@@ -0,0 +1,91 @@
+package com.sh._03_request_parameter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet("/test")
+public class RequestParameterServlet extends HttpServlet {
+    /**
+     * GET방식 요청에서 사용자입력값은 Query String (직렬화) 형태로 서버에 전달된다.
+     * - http://localhost:8080/03_request_parameter/test?name=홍길동&age=33&married=true
+     * - 위 URL에서 ?name=홍길동&age=33&married=true 부분이 Query String에 해당된다.
+     * - 쿼리 스트링은 request 객체 내부에서 HashMap으로 관리된다.
+     * - HttpServletRequest#getParamether(name):String로 처리할 수 있다.
+     * - 모든 사용자 입력값은 문자열로써 처리된다.
+     * - 단순조회 요청은 전송방식 GET을 사용한다.
+     * -
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Doget 호출");
+
+        String name = req.getParameter("name");
+        int age = Integer.parseInt(req.getParameter("age"));
+        LocalDate birthday = LocalDate.parse(req.getParameter("birthday"), DateTimeFormatter.ISO_DATE);
+        String gender = req.getParameter("gender");
+        String national = req.getParameter("national");
+        String[] hobbies = req.getParameterValues("hobbies");
+
+        System.out.println("name = "+ name);
+        System.out.println("age = "+ age);
+        System.out.println("birthday = "+ birthday);
+        System.out.println("gender = "+ gender);
+        System.out.println("national = "+ national);
+        Arrays.stream(hobbies).forEach(System.out::println);
+    }
+
+    /**
+     * Post요청에서 사용자 입력값은 http 요청 메세지의 본문 영역에 작성된다 (url에 노출되지 않는다)
+     * 동일하게 QueryString형태로 전달된다
+     * DML쿼리로 처리되어야 하는 요청은 전송방식 POST를 사용한다
+     *
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost호출");
+        //인코딩된 값 확인하기
+        System.out.println(req.getCharacterEncoding());
+        //인코딩 설정하기
+        req.setCharacterEncoding("UTF-8");
+
+
+        String name = req.getParameter("name");
+        int age = Integer.parseInt(req.getParameter("age"));
+        LocalDate birthday = LocalDate.parse(req.getParameter("birthday"), DateTimeFormatter.ISO_DATE);
+        String gender = req.getParameter("gender");
+        String national = req.getParameter("national");
+        String[] hobbies = req.getParameterValues("hobbies");
+/*
+        System.out.println("name = "+ name);
+        System.out.println("age = "+ age);
+        System.out.println("birthday = "+ birthday);
+        System.out.println("gender = "+ gender);
+        System.out.println("national = "+ national);
+        Arrays.stream(hobbies).forEach(System.out::println);
+
+ */
+        //ParameterMap을 통해서 사용자 입력값 순회
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        parameterMap.forEach((key, value) ->{
+            System.out.println(key + " = " + Arrays.toString(value));
+        });
+    }
+}
