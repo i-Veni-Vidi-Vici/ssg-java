8ec8cb63b480513bd80c67a971f9a9fa81eed83b
Hong-ji-min <jmh907@naver.com>
2024-06-05 17:10:04 +0900

    Feat : forward #8

--- HongJimin/_06_forward_response/src/main/java/com/sh/_06_forward_response/PrintServlet.java
+++ HongJimin/_06_forward_response/src/main/java/com/sh/_06_forward_response/PrintServlet.java
@@ -1,19 +1,50 @@
- package com.sh._06_forward_response;
-import jakarta.servlet.*;
+package com.sh._06_forward_response;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/print")
 public class PrintServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //사용자 입력값 가져오기
+        String category = request.getParameter("category");
+        System.out.println("PrintServlet : " + category );
+        // 업무로직 결과 Request 속성에서 menus 가져오기
+        List<String> menus = (List<String>) request.getAttribute("menus");
+        System.out.println("PrintServlet : " + menus);
+        String menuLi = menus.stream()
+                             .reduce("", (html, menu) -> html + "<li>" + menu + "</li>");
 
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // 3. 응답 html 작성하기
+        // 응답 헤더
+        response.setContentType("text/html; charset=UTF-8");
+        // 응답 바디
+        PrintWriter out = response.getWriter();
+        String html = """
+                    <!DOCTYPE html>
+                        <html>
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>카테고리별 메뉴</title>
+                            </head>
+                            <body>
+                                <h1>카테고리별 메뉴</h1>
+                                <h2>선택하신 카테고리는 %s 입니다.</h2>
+                                <ul>%s</ul>
+                                <button type = "button" onclick="history.back()">뒤로가기</button>
+                                <!-- client에서 요청하는 주소에는 application context부터 시작해야 한다.(절대주소) -->
+                                <a href="%s">뒤로가기</a>
+                            </body>
+                        </html>
+                """.formatted(category, menuLi, request.getContextPath());
+        out.println(html);
     }
 }
