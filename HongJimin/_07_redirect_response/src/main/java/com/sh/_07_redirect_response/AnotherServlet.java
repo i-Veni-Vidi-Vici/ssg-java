9d7fc010cbb997e0f7321a663d8a5b393206e3bf
Hong-ji-min <jmh907@naver.com>
2024-06-05 17:10:43 +0900

    Feat : redirect #9

--- HongJimin/_07_redirect_response/src/main/java/com/sh/_07_redirect_response/AnotherServlet.java
+++ HongJimin/_07_redirect_response/src/main/java/com/sh/_07_redirect_response/AnotherServlet.java
@@ -1,19 +1,46 @@
- package com.sh._07_redirect_response;
+package com.sh._07_redirect_response;
+
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/another")
 public class AnotherServlet extends HttpServlet {
+
+    /**
+     * redirect 응답 이후 요청은 GET방시으로 처리된다.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println(request.getAttribute("sample"));
+
+        // 응답 헤더
+        response.setContentType("text/html; charset=UTF-8");
+//        response.setContentType("text/plain; charset=UTF-8");
+        // 응답 바디
+        PrintWriter out = response.getWriter();
+        out.println("""
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>Another</title>
+                    </head>
+                    <body>
+                        <h1>🐱‍💻Another🐱‍💻</h1>
+                    </body>
+                </html>
+                """);
+
+
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
