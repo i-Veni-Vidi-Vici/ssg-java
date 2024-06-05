ab4962d18b603950cc674ef60bd0d8df766921a2
youngwoo2 <leeduddn98@gmail.com>
2024-06-05 17:11:28 +0900

    Feat: error page #7

---
+++ LeeYoungwoo/_08_error_page/src/main/java/com/sh/_08_error_page/TestServlet.java
@@ -0,0 +1,17 @@
+package com.sh._08_error_page;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 에러유발
+        throw new RuntimeException("주문 처리 오류");
+    }
+
+}
