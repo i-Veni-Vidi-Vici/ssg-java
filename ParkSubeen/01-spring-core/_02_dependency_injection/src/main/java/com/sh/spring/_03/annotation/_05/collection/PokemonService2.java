2b4e14e0c6c3e2af006e53f58041810d7c9f8200
subkka <beeniaa@naver.com>
2024-06-11 17:07:56 +0900

    Feat: DI(@Autowired 등) #2

---
+++ ParkSubeen/01-spring-core/_02_dependency_injection/src/main/java/com/sh/spring/_03/annotation/_05/collection/PokemonService2.java
@@ -0,0 +1,30 @@
+package com.sh.spring._03.annotation._05.collection;
+
+import com.sh.spring.common.pokemon.Pokemon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("pokemonServiceByMap")
+public class PokemonService2 {
+    private Map<String, Pokemon> pokemonMap;
+
+    /**
+     * key = beanname:String
+     * value = bean:Pokemon
+     * @param pokemonMap
+     */
+    @Autowired
+    public PokemonService2(Map<String, Pokemon> pokemonMap) {
+        this.pokemonMap = pokemonMap;
+    }
+
+    public void attack(){
+        this.pokemonMap.forEach((k, v) -> {
+            System.out.println(k + " = " + v);
+            v.attack();
+        });
+    }
+}
