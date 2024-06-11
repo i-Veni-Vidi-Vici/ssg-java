a6984bde9d140dd2226e179c9201150860e41c73
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-06-11 17:07:20 +0900

    "Feat: DI(@Autowired등) #2 "

---
+++ LeeYongjun/01-spring-core/_02_dependency_injection/src/main/java/com/sh/spring/common/pokemon/Squirtle.java
@@ -0,0 +1,11 @@
+package com.sh.spring.common.pokemon;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Squirtle implements Pokemon{
+    @Override
+    public void attack() {
+        System.out.println("꼬부기~ 물대포");
+    }
+}
