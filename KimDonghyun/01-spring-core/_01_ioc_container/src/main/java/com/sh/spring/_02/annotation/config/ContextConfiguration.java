dec00cf8d204c93223feb23d311d74d25ad81c77
shqkel <shqkel1863@gmail.com>
2024-06-11 17:06:22 +0900

    Feat: ioc container #1

---
+++ KimDonghyun/01-spring-core/_01_ioc_container/src/main/java/com/sh/spring/_02/annotation/config/ContextConfiguration.java
@@ -0,0 +1,35 @@
+package com.sh.spring._02.annotation.config;
+
+import com.sh.spring.common.member.MemberDao;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ComponentScan(basePackages) 하위에 등록된 @Component클래스를 빈으로 등록
+ */
+@Configuration("configurationAnnotation")
+// @ComponentScan(basePackages = "com.sh.spring.common.member") // basePackages(value)속성
+@ComponentScan(
+    basePackages = "com.sh.spring.common.member",
+    excludeFilters = {
+        // 1. 타입으로 제외
+//        @ComponentScan.Filter(
+//            type = FilterType.ASSIGNABLE_TYPE,
+//            classes = {MemberDao.class}
+//        )
+        // 2. Annotation으로 제외 (@Component, @Controller, @Service, @Repository)
+//        @ComponentScan.Filter(
+//            type = FilterType.ANNOTATION,
+//            classes = Component.class
+//        )
+        // 3. 정규표현식으로 제외
+//        @ComponentScan.Filter(
+//            type = FilterType.REGEX,
+//            pattern = {"com.sh.spring.common.member.*Dao"}
+//        )
+    }
+)
+public class ContextConfiguration {
+}
