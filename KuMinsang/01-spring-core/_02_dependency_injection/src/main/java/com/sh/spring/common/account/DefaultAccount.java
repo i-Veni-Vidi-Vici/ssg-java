4d9f5234a5ca0f290172b07c32e800fe15c8e945
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-06-11 17:18:43 +0900

    Feat: DI(@Autowired등) #2

---
+++ KuMinsang/01-spring-core/_02_dependency_injection/src/main/java/com/sh/spring/common/account/DefaultAccount.java
@@ -0,0 +1,40 @@
+package com.sh.spring.common.account;
+
+import lombok.Data;
+
+@Data
+public class DefaultAccount implements Account{
+    private final int bankCode;
+    private final String accountNo;
+    private long balance;
+
+    @Override
+    public String getBalanceInfo() {
+        return this.accountNo + "계좌의 현재 잔액은 " + this.balance + "원입니다.";
+    }
+
+    @Override
+    public String deposit(int money) {
+        String result = "";
+        if(money > 0){
+            this.balance += money;
+            result = money + "원이 입금되었습니다.";
+        }else{
+            result = "유효하지 않은 입금액입니다.";
+        }
+        return result;
+    }
+
+    @Override
+    public String withdraw(int money) {
+        String result = "";
+        if(money <= this.balance){
+            this.balance -= money;
+            result = money + "원이 출금되었습니다";
+        }else{
+            result = "잔액이 부족합니다";
+        }
+        return result;
+    }
+}
+
