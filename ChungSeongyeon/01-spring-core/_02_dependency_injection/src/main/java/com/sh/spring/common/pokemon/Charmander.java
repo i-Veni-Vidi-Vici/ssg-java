dd5c2f7d1d44b4f183f3330f047223ba3bbf6d36
seongyeongit <wjd12118@nate.com>
2024-06-11 17:07:21 +0900

    Feat: DI(@Autowired 등) #2

---
+++ ChungSeongyeon/01-spring-core/_02_dependency_injection/src/main/java/com/sh/spring/common/pokemon/Charmander.java
@@ -0,0 +1,19 @@
+package com.sh.spring.common.pokemon;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Primary
+ * - 해당 빈 클래스에 직접 작성
+ * - @Autowired에 의해서 동일한 타입의 여러 개의 빈이 검색되는 경우, 우선 바인딩할 빈을 선언
+ * - 같은 타입에서 @Primary는 하나만 사용해야 한다.
+ */
+@Component
+//@Primary
+public class Charmander implements Pokemon{
+    @Override
+    public void attack() {
+        System.out.println("파이리~ 불꽃공격! 🔥🔥🔥");
+    }
+}
