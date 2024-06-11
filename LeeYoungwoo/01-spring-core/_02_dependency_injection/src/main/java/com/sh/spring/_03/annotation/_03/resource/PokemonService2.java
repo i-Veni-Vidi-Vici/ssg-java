77038e78a6f93c3e16b4820c89dca72ccef3ec8e
youngwoo2 <leeduddn98@gmail.com>
2024-06-11 17:07:17 +0900

    Feat: DI(Autowired...) #2

---
+++ LeeYoungwoo/01-spring-core/_02_dependency_injection/src/main/java/com/sh/spring/_03/annotation/_03/resource/PokemonService2.java
@@ -0,0 +1,22 @@
+package com.sh.spring._03.annotation._03.resource;
+
+import com.sh.spring.common.pokemon.Pokemon;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("pokemonServiceByResource2")
+public class PokemonService2 {
+    /**
+    @Resource는 스프링이 아닌 자바 스펙이다.
+      - name 속성으로 특정 빈을 한정할 수 있다.
+     */
+
+    @Resource
+    private List<Pokemon> pokemons;
+
+    public void attack() {
+        this.pokemons.forEach(Pokemon::attack);
+    }
+}
