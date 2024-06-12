8b725610e6bfba98c4cb6d7e7a5a59635a9b8a2f
hurryduck <tigeryck0812@gmail.com>
2024-06-12 17:16:00 +0900

    Feat: Bean(scope, init/destroy, properties, i18n) #3

---
+++ KimYeonChan/01-spring-core/_03_bean/src/main/java/com/sh/spring/_03/properties/_02/i18n/ContextConfiguration.java
@@ -0,0 +1,26 @@
+package com.sh.spring._03.properties._02.i18n;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration("contextConfigurationI18N")
+public class ContextConfiguration {
+    @Bean
+    public MessageSource messageSource() {
+        // 사용자 로케일에 따라 메세지를 재로딩하는 MessageSource 인터페이스의 구현 클래스
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        // 다국어 처리된 message properties
+        messageSource.setBasename("i18n/message"); // src/main/resources
+        // 메세지 캐싱시간
+        messageSource.setCacheSeconds(10);
+        // 기본인코딩 설정
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    @Bean
+    public MessageService messageService() {
+        return new MessageService(messageSource());
+    }
+}
