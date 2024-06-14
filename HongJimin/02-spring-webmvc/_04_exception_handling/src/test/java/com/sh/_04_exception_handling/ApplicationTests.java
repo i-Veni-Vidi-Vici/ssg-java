2f17806f98b1d6c358c35a3ca46f2d9e095b447a
Hong-ji-min <jmh907@naver.com>
2024-06-14 17:07:50 +0900

    Feat : Exception Handling (@Controller-level @ExceptionHandler) #63

---
+++ HongJimin/02-spring-webmvc/_04_exception_handling/src/test/java/com/sh/_04_exception_handling/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh._04_exception_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
