70fe4146183b8fc35efe4c5b9c0ae8d1dea4501b
subkka <beeniaa@naver.com>
2024-06-17 17:15:10 +0900

    Update: 전역예외처리, 파일로그 #63

--- ParkSubeen/02-spring-webmvc/_04_exception_handling/src/main/java/com/sh/_04_exception_handling/IndexController.java
+++ ParkSubeen/02-spring-webmvc/_04_exception_handling/src/main/java/com/sh/_04_exception_handling/IndexController.java
@@ -18,10 +18,16 @@ public class IndexController {
         return "result";
     }
 
-    // error가 발생하면 발생한 곳에서 자동으로 forward 처리
+    /**
+     * 특정 컨트롤러 하위에서 던져진 예외클래스 별로 @ExceptionHandler를 작성할 수 있다.
+     * error가 발생하면 발생한 곳에서 자동으로 forward 처리
+     * @param e
+     * @param model
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     public String exception(Exception e, Model model) {
-        log.error(e.getMessage());
+        log.error(e.getMessage(), e); // 예외스택을 로그 확인
         model.addAttribute("message", "치즈가 상했습니다...");
         return "error";
     }
