4938d96c3acbc64bf04890f3122b061cd0b47cd5
cstangga <cstangga@naver.com>
2024-06-20 16:21:12 +0900

    Feat : Spring CRUD 메뉴목록조회/등록 #68

---
+++ ChoiChanguk/03-spring-mybatis/_03_spring_crud/src/main/java/com/sh/_03_spring_crud/menu/model/service/MenuQueryService.java
@@ -0,0 +1,42 @@
+package com.sh._03_spring_crud.menu.model.service;
+
+import com.sh._03_spring_crud.menu.model.dao.MenuMapper;
+import com.sh._03_spring_crud.menu.model.dto.CategoryDto;
+import com.sh._03_spring_crud.menu.model.dto.MenuDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true) // 하위 메소드에서는 읽기만 가능, 쓰기작업은 에러가 난다
+public class MenuQueryService {
+    // 생성자 의존주입 처리
+    private final MenuMapper menuMapper;
+
+
+    public List<MenuDto> findAll() {
+        // 매퍼(매퍼인터페이스를 구현한 빈) 가져오기
+        return menuMapper.findAll();
+    }
+
+    public MenuDto findByMenuCode(Long menuCode) {
+        return menuMapper.findByMenuCode(menuCode);
+    }
+
+    public List<MenuDto> findByMenuCategoryCode(Long categoryCode) {
+        return menuMapper.findByCategoryCode(categoryCode);
+    }
+
+    public int insertMenu(MenuDto menuDto) {
+        return menuMapper.insertMenu(menuDto);
+    }
+
+    public List<CategoryDto> findAAllCategory()
+    {
+        return menuMapper.findAllCategory();
+    }
+}
---
+++ ChoiChanguk/03-spring-mybatis/_03_spring_crud/src/test/java/com/sh/_03_spring_crud/menu/model/dao/MenuMapperTest.java
@@ -0,0 +1,105 @@
+package com.sh._03_spring_crud.menu.model.dao;
+
+import com.sh._03_spring_crud.menu.model.dto.CategoryDto;
+import com.sh._03_spring_crud.menu.model.dto.MenuDto;
+import com.sh._03_spring_crud.menu.model.dto.OrderableStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MenuMapperTest {
+    @Autowired
+    private MenuMapper menuMapper;
+
+    @Test
+    @DisplayName("메뉴 전체 조회")
+    void test1() {
+        // given
+        // when
+        List<MenuDto> menus= menuMapper.findAll();
+        System.out.println(menus);
+
+        // then
+        assertThat(menus)
+                .isNotNull()
+                .isNotEmpty()
+                .allMatch((menu)->menu!=null);
+    }
+    @Test
+    @DisplayName("메뉴 한 건 조회")
+    void test2() {
+        // given
+        Long menuCode = 1L;
+        // when
+        MenuDto menu = menuMapper.findByMenuCode(menuCode);
+        // then
+        assertThat(menu)
+                .isNotNull()
+                .satisfies(
+                        (_menu) -> assertThat(_menu.getMenuCode()).isEqualTo(menuCode),
+                        (_menu) -> assertThat(_menu.getMenuName()).isNotNull(),
+                        (_menu) -> assertThat(_menu.getMenuPrice()).isPositive(),
+                        (_menu) -> assertThat(_menu.getOrderableStatus()).satisfiesAnyOf(
+                                (orderableStatus) -> assertThat(orderableStatus).isEqualTo(OrderableStatus.Y),
+                                (orderableStatus) -> assertThat(orderableStatus).isEqualTo(OrderableStatus.N)
+                        )
+                );
+    }
+
+    @Test
+    @DisplayName("카테고리별 메뉴 조회")
+    void test3() {
+        // given
+        Long categoryCode=4L;
+
+        // when
+        List<MenuDto> menu=menuMapper.findByCategoryCode(categoryCode);
+
+        System.out.println("menu = " + menu);
+        for(MenuDto menuDto:menu)
+        {
+            System.out.println("menuDto.getMenuName() = " + menuDto.getMenuName());
+            System.out.println("menuDto.getMenuCode() = " + menuDto.getMenuCode());
+            System.out.println("menuDto.getMenuPrice() = " + menuDto.getMenuPrice());
+            System.out.println("menuDto.getCategoryCode() = " + menuDto.getCategoryCode());
+            System.out.println("menuDto.getOrderableStatus() = " + menuDto.getOrderableStatus());
+        }
+
+        // then
+        assertThat(menu).isNotNull();
+    }
+    @Test
+    @DisplayName("메뉴 등록")
+    void test4() {
+        // given
+        String menuName = "녹차막국수";
+        int menuPrice=3000;
+        int categoryCode=4;
+        OrderableStatus orderableStatus= OrderableStatus.Y;
+        MenuDto menuDto=new MenuDto(null,menuName,menuPrice,categoryCode,orderableStatus);
+
+        int result=menuMapper.insertMenu(menuDto);
+        // when
+        assertThat(result).isEqualTo(1);
+        // then
+    }
+
+    @Test
+    @DisplayName("카테고리 코드 출력")
+    void test5() {
+        // given
+        // when
+        List<CategoryDto> categories= menuMapper.findAllCategory();
+        for(CategoryDto category:categories)
+            System.out.println("category = " + category);
+        assertThat(categories).isNotNull();
+        // then
+    }
+
+}
