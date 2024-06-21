ea93568503ecbd7e972e0367b5d6a2b9a2743923
hurryduck <tigeryck0812@gmail.com>
2024-06-21 16:49:47 +0900

    feat: 비동기 통신 #161 #162

---
+++ KimYeonChan/04-spring-ajax/_02_json/src/main/java/com/sh/_02_json/Pet.java
@@ -0,0 +1,12 @@
+package com.sh._02_json;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+    String name;
+}
