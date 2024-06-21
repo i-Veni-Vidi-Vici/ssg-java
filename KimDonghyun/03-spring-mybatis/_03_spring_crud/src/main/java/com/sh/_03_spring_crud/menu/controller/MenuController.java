09be7be526525e0a17c4a38a5cb32c8b50414817
shqkel <shqkel1863@gmail.com>
2024-06-21 09:41:23 +0900

    Feat: 실습문제풀이(카테고리명 가져오기) 조인쿼리/스칼라서브쿼리 #149

--- KimDonghyun/03-spring-mybatis/_03_spring_crud/src/main/java/com/sh/_03_spring_crud/menu/controller/MenuController.java
+++ KimDonghyun/03-spring-mybatis/_03_spring_crud/src/main/java/com/sh/_03_spring_crud/menu/controller/MenuController.java
@@ -1,6 +1,7 @@
 package com.sh._03_spring_crud.menu.controller;
 
 import com.sh._03_spring_crud.menu.model.dto.CategoryDto;
+import com.sh._03_spring_crud.menu.model.dto.MenuCategoryDto;
 import com.sh._03_spring_crud.menu.model.dto.MenuDto;
 import com.sh._03_spring_crud.menu.model.dto.MenuRegistDto;
 import com.sh._03_spring_crud.menu.model.service.MenuCommandService;
@@ -29,7 +30,8 @@ public class MenuController {
     public void list(Model model) {
         log.info("GET /menu/list");
         // DB에서 메뉴목록을 조회(List<MenuDto>)후에 view단 전달해야 한다.
-        List<MenuDto> menus = menuQueryService.findAll();
+//        List<MenuDto> menus = menuQueryService.findAll();
+        List<MenuCategoryDto> menus = menuQueryService.findAll2();
         log.debug("menus = {}", menus);
         model.addAttribute("menus", menus);
     }
