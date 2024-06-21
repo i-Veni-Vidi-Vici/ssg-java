a5a33f0277ef8cae1d428ffe6dd218e34222ae80
jyna120 <jyna120@naver.com>
2024-06-21 16:48:38 +0900

    Feat: 비동기통신, XMLHttpRequest, jQuery.ajax(), json #161 #162

---
+++ NaJiyoung/04-spring-ajax/_01_spring_ajax/src/main/java/com/sh/_01_spring_ajax/IndexController.java
@@ -0,0 +1,27 @@
+package com.sh._01_spring_ajax;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+    @GetMapping(path = "/first", produces = "text/plain; charset=UTF-8")
+    @ResponseBody // 이 핸들러의 반환객체를 직접 http 응답 메세지 본문(body)에 작성하라.
+    public String first(@RequestParam String username) {
+        log.debug("username = {}", username);
+        return "🍖🍖🍖 %s, 잘 지내니? 너에게 고기를 보낸다.".formatted(username); // ViewName이 아니라 응답할 데이터 그 자체
+    }
+
+    @PostMapping(path = "/first", produces = "text/plain; charset=UTF-8")
+    @ResponseBody // 이 핸들러의 반환객체를 직접 http 응답 메세지 본문(body)에 작성하라.
+    public String first2(@RequestParam String username) {
+        log.debug("username = {}", username);
+        return "🥩🥩🥩 %s, 잘 지내니? 저번 고기는 어땠니? 이번엔 소고기야.".formatted(username); // ViewName이 아니라 응답할 데이터 그 자체
+    }
+}
