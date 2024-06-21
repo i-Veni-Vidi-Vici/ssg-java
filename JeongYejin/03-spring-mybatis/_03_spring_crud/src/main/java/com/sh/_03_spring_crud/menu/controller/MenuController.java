bbbf4044f787f33506badaa514e182bb6145a46e
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-06-21 11:03:34 +0900

    Update: 메뉴 한건 조회 #68

--- JeongYejin/03-spring-mybatis/_03_spring_crud/src/main/java/com/sh/_03_spring_crud/menu/controller/MenuController.java
+++ JeongYejin/03-spring-mybatis/_03_spring_crud/src/main/java/com/sh/_03_spring_crud/menu/controller/MenuController.java
@@ -11,10 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -60,5 +58,12 @@ public class MenuController {
         int result = menuCommandService.insertMenu(menuDto);
         redirectAttributes.addFlashAttribute("message", "메뉴를 성공적으로 등록했습니다.👏👏👏");
         return "redirect:/menu/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable Long id) {
+        MenuDto menuDto = menuQueryService.findByMenuCode(id);
+        model.addAttribute("menu", menuDto);
+        return "menu/detail";
     }
 }
