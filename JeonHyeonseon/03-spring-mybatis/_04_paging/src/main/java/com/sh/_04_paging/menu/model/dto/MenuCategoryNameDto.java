cdc743d8d94d54e5bb6f525ea7be6eec61bc59be
jeonhyeonseon <good_pretty@naver.com>
2024-06-24 12:50:27 +0900

    Feat: 페이징처리, 주문 중첩 DTO #175 #68

---
+++ JeonHyeonseon/03-spring-mybatis/_04_paging/src/main/java/com/sh/_04_paging/menu/model/dto/MenuCategoryNameDto.java
@@ -0,0 +1,18 @@
+package com.sh._04_paging.menu.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class MenuCategoryNameDto extends MenuDto{
+    private String categoryName; // 스칼라 서브쿼리로 값대입
+
+    // 부모필드(MenuDto)까지 포함해서 생성자를 하나 만들었음.
+    public MenuCategoryNameDto(Long menuCode, String menuName, int menuPrice, int categoryCode, OrderableStatus orderableStatus, String categoryName) {
+        super(menuCode, menuName, menuPrice, categoryCode, orderableStatus);
+        this.categoryName = categoryName;
+    }
+}
