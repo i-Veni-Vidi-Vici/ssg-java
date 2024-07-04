42f2c94cfd0d7c73cb063c2e4db426dbbdbbc017
hurryduck <tigeryck0812@gmail.com>
2024-07-04 10:24:41 +0900

    Feat(_01_persistence_context): Entity 객체 생명주기 #4

--- KimYeonChan/_01_persistence_contest/src/test/java/com/sh/app/_03/persistence/context/EntityLifeCycleTest.java
+++ KimYeonChan/_01_persistence_contest/src/test/java/com/sh/app/_03/persistence/context/EntityLifeCycleTest.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class EntityLifeCycleTest {
     private static EntityManagerFactory factory;
@@ -61,7 +62,7 @@ public class EntityLifeCycleTest {
     void test2() {
         // given
         Menu menu = new Menu();
-        menu.setMenuCode(200L);
+//        menu.setMenuCode(200L);
         menu.setMenuName("홍어쉐이크");
         menu.setMenuPrice(10_000_000);
         menu.setCategoryCode(4L);
@@ -92,6 +93,101 @@ public class EntityLifeCycleTest {
         Menu menu2 = entityManager.find(Menu.class, menuCode);
 
         // then
-        
+
+    }
+
+    /**
+     * 영속 상태의 entity 객체를 더 이상 영속 컨텍스트에서 관리하지 않도록 하는 것이다.
+     */
+    @Test
+    @DisplayName("준영속 detach")
+    void test4() {
+        // given
+        Menu menu = entityManager.find(Menu.class, 10L);
+        Menu menu1 = entityManager.find(Menu.class, 10L);
+        // when
+        entityManager.detach(menu);
+        Menu menu2 = entityManager.find(Menu.class, 10L);
+
+        // then
+        assertThat(entityManager.contains(menu)).isFalse();
+        System.out.println(System.identityHashCode(menu));
+        System.out.println(System.identityHashCode(menu1));
+        System.out.println(System.identityHashCode(menu2));
+    }
+
+    /**
+     * 모든 영속된 엔티티객체를 영속성 컨텍스트로부터 제거한다.
+     */
+    @Test
+    @DisplayName("준영속 clear")
+    void test5() {
+        // given
+        Menu menu10 = entityManager.find(Menu.class, 10L);
+        Menu menu11 = entityManager.find(Menu.class, 11L);
+        assertThat(entityManager.contains(menu10)).isTrue();
+        assertThat(entityManager.contains(menu11)).isTrue();
+        // when
+        entityManager.clear();
+        // then
+        assertThat(entityManager.contains(menu10)).isFalse();
+        assertThat(entityManager.contains(menu11)).isFalse();
+    }
+
+    /**
+     * remove를 호출하면, 단순히 영속성컨텍스테서 제거뿐 아니라 db로 delete 요청까지 질의함
+     * flush: 영속성 컨텍스트의 변경사항으로 실제 DB에 동기화하는 작업
+     */
+    @Test
+    @DisplayName("삭제 remove")
+    void test6() {
+        // given
+        Menu menu = entityManager.find(Menu.class, 10L);
+        // when
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        try {
+            entityManager.remove(menu); // 영속성 컨텍스트에서 삭제 처리
+//            entityManager.flush(); // delete 쿼리 질의
+//            transaction.commit(); // commit
+            transaction.commit(); // delete 질의
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+        // then
+        assertThat(entityManager.contains(menu)).isFalse();
+    }
+
+    /**
+     * 준영속객체를 다시 영속컨텍스트에 포함시킬수 있다.
+     * - 동일한 pk를 가진 엔티티객체가 없다면 사입
+     * - 동일한 pk를 가진 엔티티객체가 있다면 수정(갱신)
+     */
+    @Test
+    @DisplayName("다시 영속시키기 merge")
+    void test7() {
+        // given
+        Menu menu = entityManager.find(Menu.class, 10L);
+        entityManager.detach(menu);
+        assertThat(entityManager.contains(menu)).isFalse();
+        // when
+        // pk가 10인 Menu 객체를 다시 조회후에 menu10 객체와 병합 시도한다.
+        entityManager.merge(menu);
+        // then
+        assertThat(entityManager.contains(menu)).isTrue();
+    }
+
+    @Test
+    @DisplayName("영속성컨테스트 종료 close")
+    void test8() {
+        // given
+        Menu menu = entityManager.find(Menu.class, 11L);
+        // when
+        entityManager.close();
+        // then
+        assertThatThrownBy(() -> {
+            entityManager.find(Menu.class, 11L);
+        }).isInstanceOf(IllegalStateException.class);
     }
 }
