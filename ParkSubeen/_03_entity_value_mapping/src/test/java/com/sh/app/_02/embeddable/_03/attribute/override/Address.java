554638af066c9122eabfe5589c2ab6c77649abba
subkka <beeniaa@naver.com>
2024-07-05 16:22:02 +0900

    Feat(_03_entity_value_mapping): Aggregate/Entity/VO구분, @Embeddable, @ElementCollection #11 #12

---
+++ ParkSubeen/_03_entity_value_mapping/src/test/java/com/sh/app/_02/embeddable/_03/attribute/override/Address.java
@@ -0,0 +1,21 @@
+package com.sh.app._02.embeddable._03.attribute.override;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable // Embeddable 어노테이션은 NoArgsConstructor 어노테이션과 Serializable을 임플리먼츠 해야한다.
+@Data
+@Setter(AccessLevel.PRIVATE) // 외부에서 값을 임의로 변경하는 것을 방지
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address implements Serializable {
+    @Column(name = "addr1")
+    private String address1;
+    @Column(name = "addr2")
+    private String address2;
+    @Column(name = "zip_code")
+    private String zipCode;
+}
