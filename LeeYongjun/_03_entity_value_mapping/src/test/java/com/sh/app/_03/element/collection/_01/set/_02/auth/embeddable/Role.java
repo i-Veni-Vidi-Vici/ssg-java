eb298a470faf05513e42262a92258f24a4209ad5
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-07-09 17:09:06 +0900

    Feat: VO Collection Mapping, 상속 #12 #13

---
+++ LeeYongjun/_03_entity_value_mapping/src/test/java/com/sh/app/_03/element/collection/_01/set/_02/auth/embeddable/Role.java
@@ -0,0 +1,37 @@
+package com.sh.app._03.element.collection._01.set._02.auth.embeddable;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity(name = "Role0302")
+@Table(name = "tbl_role_0302")
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    private String id;
+    private String name;
+
+    /**
+     * <pre>
+     * fetch
+     * - fetch = FetchType.LAZY (기본값) Role객체 조회가 아닌 실제 permission 사용직전 조회
+     * - fetch = FetchType.EAGER : Role객체 조회시 함께 조회
+     * </pre>
+     */
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+        name = "tbl_role_permission_0302",
+        joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Permission> permissions;
+
+    public void changePermissions(Set<Permission> newPermissions) {
+        this.permissions = newPermissions;
+    }
+}
