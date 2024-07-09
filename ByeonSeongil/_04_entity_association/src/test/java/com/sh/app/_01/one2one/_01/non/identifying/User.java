d70c42c18447c24429f89bf5325d83fb2cd29e2d
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-07-09 16:15:42 +0900

    feat(_04_entity_association): create association #14

---
+++ ByeonSeongil/_04_entity_association/src/test/java/com/sh/app/_01/one2one/_01/non/identifying/User.java
@@ -0,0 +1,26 @@
+package com.sh.app._01.one2one._01.non.identifying;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name =  "tbl_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter(AccessLevel.PRIVATE)
+@Builder
+public class User {
+    @Id
+    private String email;
+    private String name;
+    @Column(name="create_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
