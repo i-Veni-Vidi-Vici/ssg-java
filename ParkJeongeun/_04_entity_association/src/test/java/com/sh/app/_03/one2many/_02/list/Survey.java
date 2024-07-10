66fec7d8bf830d80411171f75de553f874cfb73a
jeongeun1 <wjddms6188@gmail.com>
2024-07-10 16:37:41 +0900

    Feat(_04_entity association): 연관관계, 영속성전이 #14

---
+++ ParkJeongeun/_04_entity_association/src/test/java/com/sh/app/_03/one2many/_02/list/Survey.java
@@ -0,0 +1,39 @@
+package com.sh.app._03.one2many._02.list;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_survey")
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Builder
+public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @OneToMany
+    @JoinColumn(name = "survey_id") // tbl_inquiry.survey_id컬럼(FK)이 tbl_survey.id컬럼(PK)을 참조
+    @OrderColumn(name = "inquiry_idx")
+    @Builder.Default
+    private List<Inquiry> inquiries = new ArrayList<>();
+
+    public Survey(Long id, String title, List<Inquiry> inquiries) {
+        this.id = id;
+        this.title = title;
+        this.inquiries = new ArrayList<>(inquiries);
+    }
+
+    public void addInquiry(Inquiry inquiry) {
+        this.inquiries.add(inquiry);
+    }
+
+    public void removeInquiry(Inquiry inquiry) {
+        this.inquiries.remove(inquiry);
+    }
+}
