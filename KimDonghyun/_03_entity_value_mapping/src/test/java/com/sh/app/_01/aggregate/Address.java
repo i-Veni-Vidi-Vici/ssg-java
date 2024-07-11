f829c603fbf8daf61e6b8b71722c7535e8894de8
shqkel <shqkel1863@gmail.com>
2024-07-11 09:34:10 +0900

    Fix: Address관련 오타수정#11

--- KimDonghyun/_03_entity_value_mapping/src/test/java/com/sh/app/_01/aggregate/Address.java
+++ KimDonghyun/_03_entity_value_mapping/src/test/java/com/sh/app/_01/aggregate/Address.java
@@ -5,12 +5,11 @@ import lombok.*;
 
 import java.io.Serializable;
 
-@Embeddable
 @Data
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor
 @AllArgsConstructor
-public class Address implements Serializable {
+public class Address {
     private String address1;
     private String address2;
     private String zipCode;
