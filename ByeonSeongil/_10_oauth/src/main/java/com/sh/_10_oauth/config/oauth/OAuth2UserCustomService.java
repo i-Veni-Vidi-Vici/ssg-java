ee9143fe6c70422caf23107021f9c2e3af0932e3
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-07-25 17:52:38 +0900

    a

--- ByeonSeongil/_10_oauth/src/main/java/com/sh/_10_oauth/config/oauth/OAuth2UserCustomService.java
+++ ByeonSeongil/_10_oauth/src/main/java/com/sh/_10_oauth/config/oauth/OAuth2UserCustomService.java
@@ -18,7 +18,7 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User t commit  = super.loadUser(userRequest);
+        OAuth2User user = super.loadUser(userRequest);
         saveOrUpdate(user);
         return user;
     }
