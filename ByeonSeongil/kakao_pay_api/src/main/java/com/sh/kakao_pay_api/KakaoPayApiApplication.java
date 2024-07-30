0d39a2033aab9dd944ebf9f8aa26b0efbd003b98
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-07-30 17:28:25 +0900

    chore(kakao_pay_api): create project setting

---
+++ ByeonSeongil/kakao_pay_api/src/main/java/com/sh/kakao_pay_api/KakaoPayApiApplication.java
@@ -0,0 +1,13 @@
+package com.sh.kakao_pay_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoPayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoPayApiApplication.class, args);
+    }
+
+}
