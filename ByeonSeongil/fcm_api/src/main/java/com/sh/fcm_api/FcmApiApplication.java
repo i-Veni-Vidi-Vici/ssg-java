c7dcb5c3d6af82f87e8c03cb89b66e296b7a07b6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-01 18:01:09 +0900

    feat(fcm): create fcm api

---
+++ ByeonSeongil/fcm_api/src/main/java/com/sh/fcm_api/FcmApiApplication.java
@@ -0,0 +1,13 @@
+package com.sh.fcm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmApiApplication.class, args);
+    }
+
+}
